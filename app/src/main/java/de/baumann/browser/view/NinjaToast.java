13d3973ed58d8e88d81fb1e7aaf7d774ab5e5448
Daniel Kao <daniel.kao@linecorp.com>
2020-04-06 00:42:44 +0800

    fix crash issue when adding bookmark

--- app/src/main/java/de/baumann/browser/view/NinjaToast.java
+++ app/src/main/java/de/baumann/browser/view/NinjaToast.java
@@ -1,6 +1,5 @@
 package de.baumann.browser.view;
 
-import android.app.Activity;
 import android.content.Context;
 import android.widget.Toast;
 
@@ -8,13 +7,10 @@ import android.widget.Toast;
 public class NinjaToast {
 
     public static void show(Context context, int stringResId) {
-        show(context, context.getString(stringResId));
+        Toast.makeText(context, stringResId, Toast.LENGTH_LONG).show();
     }
 
     public static void show(Context context, String text) {
-        Activity activity = (Activity) context;
-        Toast toast = new Toast(activity.getApplicationContext());
-        toast.setText(text);
-        toast.show();
+        Toast.makeText(context, text, Toast.LENGTH_LONG).show();
     }
 }
