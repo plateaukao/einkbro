8bcb1053eb8ef7bd16f3a56556c06c5a0e365df1
Daniel Kao <daniel.kao@gmail.com>
2021-03-20 01:19:27 +0800

    fix history order issue; add basic reader mode

--- app/src/main/java/de/baumann/browser/view/toolbaricons/ToolbarAction.kt
+++ app/src/main/java/de/baumann/browser/view/toolbaricons/ToolbarAction.kt
@@ -1,7 +1,7 @@
 package de.baumann.browser.view.toolbaricons
 
 enum class ToolbarAction {
-    Title, Back, Refresh, Touch, PageUp, PageDown, TabCount, Font, Settings, Bookmark, IconSetting, VerticalLayout;
+    Title, Back, Refresh, Touch, PageUp, PageDown, TabCount, Font, Settings, Bookmark, IconSetting, VerticalLayout, ReaderMode;
 
     companion object {
         fun fromOrdinal(value: Int) = values().first { it.ordinal == value }
