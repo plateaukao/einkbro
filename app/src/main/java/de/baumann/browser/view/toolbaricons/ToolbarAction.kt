3356aa14940210b7b2e953a2f4f72b1bcbfb616a
Daniel Kao <daniel.kao@gmail.com>
2021-03-22 23:05:22 +0800

    add bold font option in toolbar

--- app/src/main/java/de/baumann/browser/view/toolbaricons/ToolbarAction.kt
+++ app/src/main/java/de/baumann/browser/view/toolbaricons/ToolbarAction.kt
@@ -1,7 +1,7 @@
 package de.baumann.browser.view.toolbaricons
 
 enum class ToolbarAction {
-    Title, Back, Refresh, Touch, PageUp, PageDown, TabCount, Font, Settings, Bookmark, IconSetting, VerticalLayout, ReaderMode;
+    Title, Back, Refresh, Touch, PageUp, PageDown, TabCount, Font, Settings, Bookmark, IconSetting, VerticalLayout, ReaderMode, BoldFont;
 
     companion object {
         fun fromOrdinal(value: Int) = values().first { it.ordinal == value }
