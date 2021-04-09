6ec80eeb2724a362ba803fa182860cb8e75a7702
Daniel Kao <daniel.kao@gmail.com>
2021-04-09 21:51:36 +0800

    refactor toolbar configuration codes

--- app/src/main/java/de/baumann/browser/view/toolbaricons/ToolbarAction.kt
+++ app/src/main/java/de/baumann/browser/view/toolbaricons/ToolbarAction.kt
@@ -1,7 +1,20 @@
 package de.baumann.browser.view.toolbaricons
 
 enum class ToolbarAction {
-    Title, Back, Refresh, Touch, PageUp, PageDown, TabCount, Font, Settings, Bookmark, IconSetting, VerticalLayout, ReaderMode, BoldFont;
+    Title, // 0
+    Back, // 1
+    Refresh, // 2
+    Touch, // 3
+    PageUp, // 4
+    PageDown, // 5
+    TabCount, // 6
+    Font, // 7
+    Settings, // 8
+    Bookmark, // 9
+    IconSetting, // 10
+    VerticalLayout, // 11
+    ReaderMode, // 12
+    BoldFont; // 13
 
     companion object {
         fun fromOrdinal(value: Int) = values().first { it.ordinal == value }
