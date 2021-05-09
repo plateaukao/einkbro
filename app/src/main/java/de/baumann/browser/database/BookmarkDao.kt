32d9cd3243dcd820748c010c70fd1fd3d8236f3e
Daniel Kao <daniel.kao@gmail.com>
2021-05-10 02:21:30 +0800

    add edit and save and delete bookmarks

--- app/src/main/java/de/baumann/browser/database/BookmarkDao.kt
+++ app/src/main/java/de/baumann/browser/database/BookmarkDao.kt
@@ -1,11 +1,8 @@
 package de.baumann.browser.database
 
 import android.content.Context
-import androidx.lifecycle.LiveData
 import androidx.room.*
 import de.baumann.browser.preference.ConfigManager
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
 
 
 @Database(entities = [Bookmark::class], version = 1)
@@ -16,22 +13,22 @@ abstract class AppDatabase : RoomDatabase() {
 @Dao
 interface BookmarkDao {
     @Query("SELECT * FROM bookmarks WHERE isDirectory != 1")
-    fun getAllBookmarks(): List<Bookmark>
+    suspend fun getAllBookmarks(): List<Bookmark>
 
     @Query("SELECT * FROM bookmarks WHERE parent = :parentId")
-    fun getBookmarksByParent(parentId: Int): List<Bookmark>
+    suspend fun getBookmarksByParent(parentId: Int): List<Bookmark>
 
     @Query("SELECT COUNT(id) FROM bookmarks WHERE url = :url")
-    fun existsUrl(url: String): Int
+    suspend fun existsUrl(url: String): Int
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    fun insert(bookmark: Bookmark)
+    suspend fun insert(bookmark: Bookmark)
 
     @Delete
-    fun delete(bookmark: Bookmark)
+    suspend fun delete(bookmark: Bookmark)
 
     @Update
-    fun update(bookmark: Bookmark)
+    suspend fun update(bookmark: Bookmark)
 }
 
 class BookmarkManger(private val context: Context) {
@@ -42,45 +39,40 @@ class BookmarkManger(private val context: Context) {
 
     val bookmarkDao = database.bookmarkDao()
 
-    fun migrateOldData() {
+    suspend fun migrateOldData() {
         val config = ConfigManager(context)
         if (config.dbVersion != 1) return
 
-        GlobalScope.launch {
-
-            //add bookmarks
-            val db = BookmarkList(context).apply { open() }
-            val cursor = db.fetchAllData(context)
-            cursor.moveToFirst()
-            while (!cursor.isAfterLast) {
-                insert(
-                    Bookmark(
-                        title = cursor.getString(1),
-                        url = cursor.getString(2),
-                        isDirectory = false,
-                        parent = 0
-                    )
-                )
-                cursor.moveToNext()
-            }
-            cursor.close()
-            db.close()
+        //add bookmarks
+        val db = BookmarkList(context).apply { open() }
+        val cursor = db.fetchAllData(context)
+        cursor.moveToFirst()
+        while (!cursor.isAfterLast) {
+            insert(
+                title = cursor.getString(1),
+                url = cursor.getString(2),
+            )
+            cursor.moveToNext()
         }
+        cursor.close()
+        db.close()
 
         config.dbVersion = 1
     }
 
-    fun getBookmarks(): List<Bookmark> = bookmarkDao.getAllBookmarks()
+    suspend fun getBookmarks(): List<Bookmark> = bookmarkDao.getAllBookmarks()
 
-    fun getBookmarksByParent(parent: Int) = bookmarkDao.getBookmarksByParent(parent)
+    suspend fun getBookmarksByParent(parent: Int) = bookmarkDao.getBookmarksByParent(parent)
 
-    fun insert(bookmark: Bookmark) {
-        if (bookmarkDao.existsUrl(bookmark.url) > 0) return
+    suspend fun insert(title: String, url: String) {
+        if (existsUrl(url)) return
 
-        bookmarkDao.insert(bookmark)
+        bookmarkDao.insert(Bookmark(title, url))
     }
 
-    fun insertDirectory(title: String, parentId: Int = 0) {
+    suspend fun existsUrl(url: String): Boolean = bookmarkDao.existsUrl(url) > 0
+
+    suspend fun insertDirectory(title: String, parentId: Int = 0) {
         bookmarkDao.insert(
             Bookmark(
                 title = title,
@@ -91,7 +83,9 @@ class BookmarkManger(private val context: Context) {
         )
     }
 
-    fun delete(bookmark: Bookmark) = bookmarkDao.delete(bookmark)
+    suspend fun delete(bookmark: Bookmark) = bookmarkDao.delete(bookmark)
 
-    fun update(bookmark: Bookmark) = bookmarkDao.update(bookmark)
+    suspend fun update(bookmark: Bookmark) = bookmarkDao.update(bookmark)
+
+    fun release() = database.close()
 }
