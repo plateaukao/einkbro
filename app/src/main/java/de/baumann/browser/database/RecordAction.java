9589a40697db677ec75beebfe413557ed64ec412
Daniel Kao <daniel.kao@gmail.com>
2021-05-12 00:03:05 +0800

    remove grid item related codes

--- app/src/main/java/de/baumann/browser/database/RecordAction.java
+++ app/src/main/java/de/baumann/browser/database/RecordAction.java
@@ -14,7 +14,6 @@ import java.util.List;
 import java.util.Objects;
 
 import de.baumann.browser.unit.RecordUnit;
-import de.baumann.browser.view.GridItem;
 
 public class RecordAction {
     private SQLiteDatabase database;
@@ -51,46 +50,6 @@ public class RecordAction {
         values.put(RecordUnit.COLUMN_DOMAIN, domain.trim());
         database.insert(table, null, values);
     }
-
-    public boolean addGridItem(GridItem item) {
-        if (item == null
-                || item.getTitle() == null
-                || item.getTitle().trim().isEmpty()
-                || item.getURL() == null
-                || item.getURL().trim().isEmpty()
-                || item.getFilename() == null
-                || item.getFilename().trim().isEmpty()
-                || item.getOrdinal() < 0) {
-            return false;
-        }
-        ContentValues values = new ContentValues();
-        values.put(RecordUnit.COLUMN_TITLE, item.getTitle().trim());
-        values.put(RecordUnit.COLUMN_URL, item.getURL().trim());
-        values.put(RecordUnit.COLUMN_FILENAME, item.getFilename().trim());
-        values.put(RecordUnit.COLUMN_ORDINAL, item.getOrdinal());
-        database.insert(RecordUnit.TABLE_GRID, null, values);
-        return true;
-    }
-
-    public void updateGridItem(GridItem item) {
-        if (item == null
-                || item.getTitle() == null
-                || item.getTitle().trim().isEmpty()
-                || item.getURL() == null
-                || item.getURL().trim().isEmpty()
-                || item.getFilename() == null
-                || item.getFilename().trim().isEmpty()
-                || item.getOrdinal() < 0) {
-            return;
-        }
-        ContentValues values = new ContentValues();
-        values.put(RecordUnit.COLUMN_TITLE, item.getTitle().trim());
-        values.put(RecordUnit.COLUMN_URL, item.getURL().trim());
-        values.put(RecordUnit.COLUMN_FILENAME, item.getFilename().trim());
-        values.put(RecordUnit.COLUMN_ORDINAL, item.getOrdinal());
-        database.update(RecordUnit.TABLE_GRID, values, RecordUnit.COLUMN_URL + "=?", new String[] {item.getURL()});
-    }
-
 
     public boolean checkHistory(String url) {
         if (url == null || url.trim().isEmpty()) {
@@ -135,27 +94,6 @@ public class RecordAction {
         return false;
     }
 
-    public boolean checkGridItem(String url) {
-        if (url == null || url.trim().isEmpty()) {
-            return false;
-        }
-        Cursor cursor = database.query(
-                RecordUnit.TABLE_GRID,
-                new String[] {RecordUnit.COLUMN_URL},
-                RecordUnit.COLUMN_URL + "=?",
-                new String[] {url.trim()},
-                null,
-                null,
-                null
-        );
-        if (cursor != null) {
-            boolean result = cursor.moveToFirst();
-            cursor.close();
-            return result;
-        }
-        return false;
-    }
-
     public void deleteHistoryItemByURL (String domain) {
         if (domain == null || domain.trim().isEmpty()) { return; }
         database.execSQL("DELETE FROM "+ RecordUnit.TABLE_HISTORY + " WHERE " + RecordUnit.COLUMN_URL + " = " + "\"" + domain.trim() + "\"");
@@ -164,11 +102,6 @@ public class RecordAction {
     public void deleteHistoryItem(Record record) {
         if (record == null || record.getTime() <= 0) { return; }
         database.execSQL("DELETE FROM "+ RecordUnit.TABLE_HISTORY + " WHERE " + RecordUnit.COLUMN_TIME + " = " + record.getTime());
-    }
-
-    public void deleteGridItem(GridItem item) {
-        if (item == null || item.getURL() == null || item.getURL().trim().isEmpty()) { return; }
-        database.execSQL("DELETE FROM " + RecordUnit.TABLE_GRID + " WHERE " + RecordUnit.COLUMN_URL + " = " + "\"" + item.getURL().trim() + "\"");
     }
 
     public void deleteDomain(String domain, String table) {
@@ -196,15 +129,6 @@ public class RecordAction {
         record.setURL(cursor.getString(1));
         record.setTime(cursor.getLong(2));
         return record;
-    }
-
-    private GridItem getGridItem(Cursor cursor) {
-        GridItem item = new GridItem();
-        item.setTitle(cursor.getString(0));
-        item.setURL(cursor.getString(1));
-        item.setFilename(cursor.getString(2));
-        item.setOrdinal(cursor.getInt(3));
-        return item;
     }
 
     public List<Record> listEntries (Activity activity, boolean listAll) {
@@ -290,39 +214,6 @@ public class RecordAction {
         cursor.moveToFirst();
         while (!cursor.isAfterLast()) {
             list.add(cursor.getString(0));
-            cursor.moveToNext();
-        }
-        cursor.close();
-        return list;
-    }
-
-    public List<GridItem> listGrid(Context context) {
-
-        List<GridItem> list = new LinkedList<>();
-        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(context);
-        String sortBy = Objects.requireNonNull(sp.getString("sort_startSite", "ordinal"));
-
-        Cursor cursor;
-        cursor = database.query(
-                RecordUnit.TABLE_GRID,
-                new String[] {
-                        RecordUnit.COLUMN_TITLE,
-                        RecordUnit.COLUMN_URL,
-                        RecordUnit.COLUMN_FILENAME,
-                        RecordUnit.COLUMN_ORDINAL
-                },
-                null,
-                null,
-                null,
-                null,
-                sortBy
-        );
-        if (cursor == null) {
-            return list;
-        }
-        cursor.moveToFirst();
-        while (!cursor.isAfterLast()) {
-            list.add(getGridItem(cursor));
             cursor.moveToNext();
         }
         cursor.close();
