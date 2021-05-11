40bec27273a2c5adc860e50d75a48084c82cbfa4
Daniel Kao <daniel.kao@gmail.com>
2021-05-12 00:03:05 +0800

    remove grid item related codes

--- app/src/main/java/de/baumann/browser/fragment/UiSettingsFragment.kt
+++ app/src/main/java/de/baumann/browser/fragment/UiSettingsFragment.kt
@@ -1,22 +1,16 @@
 package de.baumann.browser.fragment
 
 import android.content.Context
-import android.content.Intent
 import android.content.SharedPreferences
 import android.content.SharedPreferences.OnSharedPreferenceChangeListener
 import android.os.Bundle
-import android.view.View
-import androidx.fragment.app.DialogFragment
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import de.baumann.browser.Ninja.R
-import de.baumann.browser.activity.Settings_DataActivity
 import de.baumann.browser.preference.ConfigManager
 import de.baumann.browser.util.Constants
 import de.baumann.browser.view.dialog.ToolbarConfigDialog
 import de.baumann.browser.view.dialog.TouchAreaDialog
-import de.baumann.browser.view.sortlistpreference.MultiSelectDragListPreference
-import de.baumann.browser.view.sortlistpreference.MultiSelectDragListPreferenceDialog
 
 class UiSettingsFragment : PreferenceFragmentCompat(), OnSharedPreferenceChangeListener {
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
