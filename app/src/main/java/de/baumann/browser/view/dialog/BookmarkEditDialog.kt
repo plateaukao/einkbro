da5dbe80f99dd7a753dec00b8a3cc4c3efe3dd3c
Daniel Kao <daniel.kao@gmail.com>
2021-05-11 23:11:57 +0800

    modify edit bookmark layout

--- app/src/main/java/de/baumann/browser/view/dialog/BookmarkEditDialog.kt
+++ app/src/main/java/de/baumann/browser/view/dialog/BookmarkEditDialog.kt
@@ -1,6 +1,8 @@
 package de.baumann.browser.view.dialog
 
+import android.app.AlertDialog
 import android.content.Context
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.widget.AdapterView
@@ -23,8 +25,8 @@ class BookmarkEditDialog(
     private val okAction: () -> Unit,
     private val cancelAction: () -> Unit,
 ) {
-    val bottomSheetDialog = BottomSheetDialog(context, R.style.BottomSheetDialog)
     val menuView = DialogEditBookmarkBinding.inflate(layoutInflater)
+
     init {
         menuView.passTitle.setText(bookmark.title)
         if (bookmark.isDirectory) {
@@ -35,47 +37,47 @@ class BookmarkEditDialog(
     }
 
     fun show() {
-        try {
-            // load all folders
-            lifecycleScope.launch {
-                val folders = bookmarkManager.getBookmarkFolders().toMutableList().apply {  add(0, Bookmark("Top", "", true)) }
-                if (bookmark.isDirectory) folders.remove(bookmark)
-
-                menuView.folderSpinner.adapter = ArrayAdapter(context, android.R.layout.simple_spinner_dropdown_item, folders)
-                menuView.folderSpinner.onItemSelectedListener = object: AdapterView.OnItemSelectedListener{
-                    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-                        bookmark.parent = folders[position].id
-                    }
-
-                    override fun onNothingSelected(parent: AdapterView<*>?) { }
-                }
-            }
-
-            menuView.actionOk.setOnClickListener {
-                try {
-                    bookmark.title = menuView.passTitle.text.toString().trim { it <= ' ' }
-                    bookmark.url = menuView.passUrl.text.toString().trim { it <= ' ' }
-                    lifecycleScope.launch {
-                        bookmarkManager.update(bookmark)
-                        okAction.invoke()
-                    }
-                } catch (e: Exception) {
-                    e.printStackTrace()
-                    NinjaToast.show(context, R.string.toast_error)
-                }
-                bottomSheetDialog.hide()
-            }
-
-            menuView.actionCancel.setOnClickListener {
-                bottomSheetDialog.cancel()
-                cancelAction.invoke()
-            }
-
-            bottomSheetDialog.setContentView(menuView.root)
-            bottomSheetDialog.show()
-        } catch (e: Exception) {
-            e.printStackTrace()
-            NinjaToast.show(context, R.string.toast_error)
+        val builder = AlertDialog.Builder(context, R.style.TouchAreaDialog).apply { setView(menuView.root) }
+        val dialog = builder.create().apply {
+            window?.setGravity(Gravity.BOTTOM)
+            window?.setBackgroundDrawableResource(R.drawable.background_with_margin)
         }
+
+        // load all folders
+        lifecycleScope.launch {
+            val folders = bookmarkManager.getBookmarkFolders().toMutableList().apply {  add(0, Bookmark("Top", "", true)) }
+            if (bookmark.isDirectory) folders.remove(bookmark)
+
+            menuView.folderSpinner.adapter = ArrayAdapter(context, android.R.layout.simple_spinner_dropdown_item, folders)
+            menuView.folderSpinner.onItemSelectedListener = object: AdapterView.OnItemSelectedListener{
+                override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                    bookmark.parent = folders[position].id
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>?) { }
+            }
+        }
+
+        menuView.actionOk.setOnClickListener {
+            try {
+                bookmark.title = menuView.passTitle.text.toString().trim { it <= ' ' }
+                bookmark.url = menuView.passUrl.text.toString().trim { it <= ' ' }
+                lifecycleScope.launch {
+                    bookmarkManager.update(bookmark)
+                    okAction.invoke()
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+                NinjaToast.show(context, R.string.toast_error)
+            }
+            dialog.hide()
+        }
+
+        menuView.actionCancel.setOnClickListener {
+            dialog.cancel()
+            cancelAction.invoke()
+        }
+
+        dialog.show()
     }
 }
