8efcda61e727d13a41d295842a582c0db7b935de
Daniel Kao <daniel.kao@gmail.com>
2021-06-15 00:29:10 +0800

    refactor context menu link dialog

--- app/src/main/java/de/baumann/browser/view/dialog/DialogManager.kt
+++ app/src/main/java/de/baumann/browser/view/dialog/DialogManager.kt
@@ -4,12 +4,17 @@ import android.app.Activity
 import android.app.Dialog
 import android.content.Context
 import android.view.Gravity
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.URLUtil
 import androidx.appcompat.app.AlertDialog
 import de.baumann.browser.Ninja.R
+import de.baumann.browser.Ninja.databinding.DialogEditExtensionBinding
 import de.baumann.browser.preference.ConfigManager
+import de.baumann.browser.unit.HelperUnit
 import de.baumann.browser.unit.ViewUnit
+import de.baumann.browser.view.NinjaToast
 
 class DialogManager(
     private val activity: Activity
@@ -34,27 +39,59 @@ class DialogManager(
         }
     }
 
+    fun showSavePdfDialog(
+        context: Context,
+        url: String,
+        savePdf: (String, String) -> Unit,
+    ) {
+        val menuView = DialogEditExtensionBinding.inflate(LayoutInflater.from(context))
+        val editTitle = menuView.dialogEdit.apply {
+            setHint(R.string.dialog_title_hint)
+            setText(HelperUnit.fileName(url))
+        }
+        val editExtension = menuView.dialogEditExtension
+        val filename = URLUtil.guessFileName(url, null, null)
+        val extension = filename.substring(filename.lastIndexOf("."))
+        if (extension.length <= 8) {
+            editExtension.setText(extension)
+        }
+        showOkCancelDialog(
+            context,
+            title = context.getString(R.string.menu_edit),
+            view = menuView.root,
+            okAction = {
+                val title = editTitle.text.toString().trim { it <= ' ' }
+                val extension = editExtension.text.toString().trim { it <= ' ' }
+                val fileName = title + extension
+                if (url == null || title.isEmpty() || extension.isEmpty() || !extension.startsWith(".")) {
+                    NinjaToast.show(context, context.getString(R.string.toast_input_empty))
+                } else {
+                    savePdf(url, fileName)
+                }
+            },
+            cancelAction = { ViewUnit.hideKeyboard(context as Activity) }
+        )
+    }
+
     fun showOkCancelDialog(
         context: Context,
         title: String? = null,
-        messageResId:Int,
+        messageResId: Int? = null,
+        view: View? = null,
         okAction: () -> Unit,
-        cancelAction: (() -> Unit)? = null)
-    {
-        AlertDialog.Builder(context, R.style.TouchAreaDialog)
-            .setMessage(messageResId)
-            .setPositiveButton(android.R.string.ok) { _, _ ->
-                okAction.invoke()
-            }
-            .setNegativeButton(android.R.string.cancel) { _, _ ->
-                cancelAction?.invoke()
-            }.apply {
+        cancelAction: (() -> Unit)? = null
+    ): Dialog {
+        val dialog = AlertDialog.Builder(context, R.style.TouchAreaDialog)
+            .setPositiveButton(android.R.string.ok) { _, _ -> okAction() }
+            .setNegativeButton(android.R.string.cancel) { _, _ -> cancelAction?.invoke() }
+            .apply {
                 title?.let { title -> setTitle(title) }
+                view?.let { setView(it) }
+                messageResId?.let { setMessage(messageResId) }
             }
-            .create().apply {
-                window?.setGravity(Gravity.BOTTOM)
-            }
-            .show()
+            .create().apply { window?.setGravity(Gravity.BOTTOM) }
+        dialog.show()
+        return dialog
     }
 
     fun showOptionDialog(
