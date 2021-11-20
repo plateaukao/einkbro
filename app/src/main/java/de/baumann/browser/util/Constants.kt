b5f9922c2884b89e8f589a67568347a55f79bc4b
Daniel Kao <daniel.kao@gmail.com>
2021-11-20 18:45:02 +0800

    --amend

--- app/src/main/java/de/baumann/browser/util/Constants.kt
+++ app/src/main/java/de/baumann/browser/util/Constants.kt
@@ -2,7 +2,7 @@ package de.baumann.browser.util
 
 class Constants {
     companion object {
-        const val DEFAULT_HOME_URL = "https://duckduckgo.com/"
+        const val DEFAULT_HOME_URL = "https://www.google.com/"
         const val ARG_LAUNCH_TOOLBAR_SETTING = "launch_toolbar_setting"
         const val MIME_TYPE_EPUB = "application/epub+zip"
         const val MIME_TYPE_PDF = "application/pdf"
