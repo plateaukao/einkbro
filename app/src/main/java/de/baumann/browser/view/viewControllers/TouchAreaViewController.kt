7425e6822d95759cfdeae18217cc6c19d2c49f20
Daniel Kao <daniel.kao@gmail.com>
2021-12-25 15:13:56 +0800

    add feature to adjust the position of touch area

--- app/src/main/java/de/baumann/browser/view/viewControllers/TouchAreaViewController.kt
+++ app/src/main/java/de/baumann/browser/view/viewControllers/TouchAreaViewController.kt
@@ -1,9 +1,11 @@
 package de.baumann.browser.view.viewControllers
 
-import android.content.Context
+import android.annotation.SuppressLint
 import android.content.SharedPreferences
 import android.graphics.Color
+import android.view.MotionEvent
 import android.view.View
+import androidx.core.view.doOnLayout
 import de.baumann.browser.Ninja.R
 import de.baumann.browser.preference.ConfigManager
 import de.baumann.browser.preference.TouchAreaType
@@ -12,7 +14,6 @@ import org.koin.core.component.inject
 import java.util.*
 
 class TouchAreaViewController(
-        private val context: Context,
         private val rootView: View,
         private val pageUpAction: () -> Unit,
         private val pageTopAction: () -> Unit,
@@ -21,6 +22,8 @@ class TouchAreaViewController(
 ) : KoinComponent {
     private lateinit var touchAreaPageUp: View
     private lateinit var touchAreaPageDown: View
+    private lateinit var touchAreaDragCustomize: View
+
     private val config: ConfigManager by inject()
 
     private val touchAreaChangeListener = SharedPreferences.OnSharedPreferenceChangeListener { _, key ->
@@ -34,15 +37,47 @@ class TouchAreaViewController(
 
         if (key == ConfigManager.K_TOUCH_AREA_TYPE) {
             updateTouchAreaType()
+            // reset offset when type is changed
+            config.touchAreaCustomizeY = 0
         }
     }
 
     init {
         config.registerOnSharedPreferenceChangeListener(touchAreaChangeListener)
         updateTouchAreaType()
+        if (config.touchAreaCustomizeY != 0) {
+            rootView.post {
+                updateTouchAreaCustomizeY(config.touchAreaCustomizeY)
+            }
+        }
     }
 
-    fun updateTouchAreaType() {
+    private fun customOnTouch(view: View, event: MotionEvent): Boolean {
+        var dY = 0F
+        when(event.actionMasked) {
+            MotionEvent.ACTION_DOWN -> {
+                dY = view.y - event.rawY
+            }
+            MotionEvent.ACTION_MOVE -> {
+                val currentViewY = event.rawY - dY - view.height
+                val customizeY = currentViewY + view.height / 2
+                updateTouchAreaCustomizeY(customizeY.toInt())
+            }
+            MotionEvent.ACTION_UP -> { }
+        }
+        return true
+    }
+
+    private fun updateTouchAreaCustomizeY(customizeY: Int) {
+        config.touchAreaCustomizeY = customizeY
+        val upDownDiffY = touchAreaPageDown.y - touchAreaPageUp.y
+        touchAreaDragCustomize.y = (customizeY - touchAreaDragCustomize.height / 2).toFloat()
+        touchAreaPageUp.y = customizeY.toFloat()
+        touchAreaPageDown.y = touchAreaPageUp.y + upDownDiffY
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    private fun updateTouchAreaType() {
         // hide current one, and reset listener
         if (this::touchAreaPageUp.isInitialized) {
             with(touchAreaPageUp) {
@@ -55,26 +90,32 @@ class TouchAreaViewController(
                 setOnLongClickListener(null)
                 setOnClickListener(null)
             }
+            with (touchAreaDragCustomize) {
+                visibility = View.INVISIBLE
+                setOnTouchListener(null)
+            }
         }
 
         when (config.touchAreaType) {
             TouchAreaType.BottomLeftRight -> {
                 touchAreaPageUp = rootView.findViewById(R.id.touch_area_bottom_left)
                 touchAreaPageDown = rootView.findViewById(R.id.touch_area_bottom_right)
+                touchAreaDragCustomize = rootView.findViewById(R.id.touch_area_bottom_drag)
             }
             TouchAreaType.MiddleLeftRight -> {
                 touchAreaPageUp = rootView.findViewById(R.id.touch_area_middle_left)
                 touchAreaPageDown = rootView.findViewById(R.id.touch_area_middle_right)
+                touchAreaDragCustomize = rootView.findViewById(R.id.touch_area_middle_drag)
             }
             TouchAreaType.Left -> {
                 touchAreaPageUp = rootView.findViewById(R.id.touch_area_left_1)
                 touchAreaPageDown = rootView.findViewById(R.id.touch_area_left_2)
+                touchAreaDragCustomize = rootView.findViewById(R.id.touch_area_left_drag)
             }
             TouchAreaType.Right -> {
                 touchAreaPageUp = rootView.findViewById(R.id.touch_area_right_1)
                 touchAreaPageDown = rootView.findViewById(R.id.touch_area_right_2)
-            }
-            else -> {
+                touchAreaDragCustomize = rootView.findViewById(R.id.touch_area_right_drag)
             }
         }
 
@@ -88,6 +129,10 @@ class TouchAreaViewController(
             if (isTouchEnabled) visibility = View.VISIBLE
             setOnClickListener { pageDownAction.invoke() }
             setOnLongClickListener { pageBottomAction.invoke(); true }
+        }
+        with(touchAreaDragCustomize) {
+            if (isTouchEnabled) visibility = View.VISIBLE
+            setOnTouchListener { view, event -> customOnTouch(view, event) }
         }
     }
 
@@ -112,6 +157,7 @@ class TouchAreaViewController(
     fun toggleTouchPageTurn(enabled: Boolean) {
         touchAreaPageUp.visibility = if (enabled) View.VISIBLE else View.INVISIBLE
         touchAreaPageDown.visibility = if (enabled) View.VISIBLE else View.INVISIBLE
+        touchAreaDragCustomize.visibility = if (enabled) View.VISIBLE else View.INVISIBLE
 
         if (enabled) showTouchAreaHint()
     }
