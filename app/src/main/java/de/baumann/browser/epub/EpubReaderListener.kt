7267bda07f32fc6422d028eda8b955251f5d8505
Daniel Kao <daniel.kao@gmail.com>
2022-02-01 16:55:49 +0800

    now selection text works

--- app/src/main/java/de/baumann/browser/epub/EpubReaderListener.kt
+++ app/src/main/java/de/baumann/browser/epub/EpubReaderListener.kt
@@ -3,7 +3,7 @@ package de.baumann.browser.epub
 interface EpubReaderListener {
     fun onPageChangeListener(ChapterNumber: Int, PageNumber: Int, ProgressStart: Float, ProgressEnd: Float)
     fun onChapterChangeListener(ChapterNumber: Int)
-    fun onTextSelectionModeChangeListner(mode: Boolean?)
+    fun onTextSelectionModeChangeListener(mode: Boolean?)
     fun onLinkClicked(url: String?)
     fun onBookStartReached()
     fun onBookEndReached()
