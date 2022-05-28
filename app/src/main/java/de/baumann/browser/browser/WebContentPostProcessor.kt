6473a2fdd43737ccfb500bfe8e127e9b7915b416
Daniel Kao <daniel.kao@gmail.com>
2022-05-28 18:17:11 +0800

    feat: remove suggested posted in fb website

--- app/src/main/java/de/baumann/browser/browser/WebContentPostProcessor.kt
+++ app/src/main/java/de/baumann/browser/browser/WebContentPostProcessor.kt
@@ -16,16 +16,16 @@ class WebContentPostProcessor {
     companion object {
         private const val facebookHideSponsoredPostsJs = """
             javascript:(function() {
-              var posts = [].filter.call(document.getElementsByTagName('article'), el => el.attributes['data-store'].value.indexOf('is_sponsored.1') >= 0); 
+              var posts = [].filter.call(document.getElementsByTagName('article'), el => el.attributes['data-store'].value.indexOf('is_sponsored.1') >= 0 || el.getElementsByTagName('header')[0].innerText == 'Suggested for you'); 
               while(posts.length > 0) { posts.pop().style.display = "none"; }
               
               var ads = Array.from(document.getElementsByClassName("bg-s3")).filter(e => e.innerText.indexOf("Sponsored") != -1);
               ads.forEach(el => {el.style.display="none"; el.nextSibling.style.display="none";el.nextSibling.nextSibling.style.display="none"});
               ads.forEach(el => {el.nextSibling.nextSibling.nextSibling.style.display="none"});
               ads.forEach(el => {el.nextSibling.nextSibling.nextSibling.nextSibling.style.display="none"});
-            
+              
             var qcleanObserver = new window.MutationObserver(function(mutation, observer){ 
-              var posts = [].filter.call(document.getElementsByTagName('article'), el => el.attributes['data-store'].value.indexOf('is_sponsored.1') >= 0); 
+              var posts = [].filter.call(document.getElementsByTagName('article'), el => el.attributes['data-store'].value.indexOf('is_sponsored.1') >= 0 || el.getElementsByTagName('header')[0].innerText == 'Suggested for you'); 
               while(posts.length > 0) { posts.pop().style.display = "none"; }
               
               var ads = Array.from(document.getElementsByClassName("bg-s3")).filter(e => e.innerText.indexOf("Sponsored") != -1);
