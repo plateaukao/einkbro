228cc6ddb5cf12c3ae1a1ace78e4859cfa0235c5
Daniel Kao <daniel.kao@gmail.com>
2022-06-09 21:02:14 +0800

    ver: update lib version; add drag and drop feature

--- app/src/main/java/de/baumann/browser/viewmodel/BookmarkViewModel.kt
+++ app/src/main/java/de/baumann/browser/viewmodel/BookmarkViewModel.kt
@@ -10,12 +10,6 @@ class BookmarkViewModel(private val bookmarkDao: BookmarkDao): ViewModel() {
     fun bookmarksByParent(parentId: Int): Flow<List<Bookmark>> = bookmarkDao.getBookmarksByParentFlow(parentId)
 }
 
-class BookmarkViewModelFactory(private val bookmarkDao: BookmarkDao) : ViewModelProvider.Factory {
-    override fun <T : ViewModel> create(modelClass: Class<T>): T {
-        if (modelClass.isAssignableFrom(BookmarkViewModel::class.java)) {
-            @Suppress("UNCHECKED_CAST")
-            return BookmarkViewModel(bookmarkDao) as T
-        }
-        throw IllegalAccessException("Unknown ViewModel class")
-    }
+class BookmarkViewModelFactory(private val bookmarkDao: BookmarkDao): ViewModelProvider.NewInstanceFactory() {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T = BookmarkViewModel(bookmarkDao) as T
 }
