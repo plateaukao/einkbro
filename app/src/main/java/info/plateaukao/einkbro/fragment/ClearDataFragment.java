9680a2be2a907eea8d3aea67b01bf9c0603e6bf7
Daniel Kao <daniel.kao@gmail.com>
2022-08-11 20:19:23 +0800

    ver: remove pref theme

--- app/src/main/java/info/plateaukao/einkbro/fragment/ClearDataFragment.java
+++ app/src/main/java/info/plateaukao/einkbro/fragment/ClearDataFragment.java
@@ -1,5 +1,6 @@
 package info.plateaukao.einkbro.fragment;
 
+import android.app.Activity;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
@@ -28,21 +29,19 @@ public class ClearDataFragment extends PreferenceFragmentCompat implements Fragm
         setPreferencesFromResource(R.xml.preference_clear, rootKey);
 
         findPreference("sp_deleteDatabase").setOnPreferenceClickListener(preference -> {
+            final Activity activity = getActivity();
             final SharedPreferences sp = getPreferenceScreen().getSharedPreferences();
-            final BottomSheetDialog dialog = new BottomSheetDialog(requireActivity());
+            final BottomSheetDialog dialog = new BottomSheetDialog(activity);
             View dialogView = View.inflate(getActivity(), R.layout.dialog_action, null);
             TextView textView = dialogView.findViewById(R.id.dialog_text);
             textView.setText(R.string.hint_database);
             Button action_ok = dialogView.findViewById(R.id.action_ok);
-            action_ok.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    dialog.cancel();
-                    getActivity().deleteDatabase("Ninja4.db");
-                    getActivity().deleteDatabase("pass_DB_v01.db");
-                    sp.edit().putBoolean("restart_changed", true).apply();
-                    getActivity().finish();
-                }
+            action_ok.setOnClickListener(view -> {
+                dialog.cancel();
+                activity.deleteDatabase("Ninja4.db");
+                activity.deleteDatabase("pass_DB_v01.db");
+                sp.edit().putBoolean("restart_changed", true).apply();
+                getActivity().finish();
             });
             Button action_cancel = dialogView.findViewById(R.id.action_cancel);
             action_cancel.setOnClickListener(new View.OnClickListener() {
