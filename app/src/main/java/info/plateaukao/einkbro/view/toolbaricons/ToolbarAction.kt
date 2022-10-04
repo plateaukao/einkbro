97cd91753886fb64015e83c81f5a4788403644a5
Daniel Kao <daniel.kao@gmail.com>
2022-10-05 00:36:20 +0800

    ui: add input icon to title, so that user knows it's for input url.

--- app/src/main/java/info/plateaukao/einkbro/view/toolbaricons/ToolbarAction.kt
+++ app/src/main/java/info/plateaukao/einkbro/view/toolbaricons/ToolbarAction.kt
@@ -10,8 +10,20 @@ enum class ToolbarAction(
 ) {
     Title(iconResId = R.drawable.icon_info, titleResId = R.string.toolbar_title), // 0
     Back(iconResId = R.drawable.icon_arrow_left_gest, titleResId = R.string.back),
-    Refresh(iconResId = R.drawable.icon_refresh, titleResId = R.string.refresh, iconActiveInfo = IconActiveInfo(true, R.drawable.ic_stop, R.drawable.icon_refresh)),
-    Touch(iconResId = R.drawable.ic_touch_enabled, titleResId = R.string.touch_turn_page,  iconActiveInfo = IconActiveInfo(true,  R.drawable.ic_touch_enabled,  R.drawable.ic_touch_disabled)),
+    Refresh(
+        iconResId = R.drawable.icon_refresh,
+        titleResId = R.string.refresh,
+        iconActiveInfo = IconActiveInfo(true, R.drawable.ic_stop, R.drawable.icon_refresh)
+    ),
+    Touch(
+        iconResId = R.drawable.ic_touch_enabled,
+        titleResId = R.string.touch_turn_page,
+        iconActiveInfo = IconActiveInfo(
+            true,
+            R.drawable.ic_touch_enabled,
+            R.drawable.ic_touch_disabled
+        )
+    ),
     PageUp(iconResId = R.drawable.ic_page_up, titleResId = R.string.page_up),
     PageDown(iconResId = R.drawable.ic_page_down, titleResId = R.string.page_down),
     TabCount(iconResId = R.drawable.icon_preview, titleResId = R.string.tab_preview),
@@ -21,7 +33,15 @@ enum class ToolbarAction(
     IconSetting(iconResId = R.drawable.ic_toolbar, titleResId = R.string.toolbars),
     VerticalLayout(iconResId = R.drawable.ic_vertical_read, titleResId = R.string.vertical_read),
     ReaderMode(iconResId = R.drawable.ic_reader, titleResId = R.string.reader_mode),
-    BoldFont(iconResId = R.drawable.ic_bold_font, titleResId = R.string.bold_font, iconActiveInfo = IconActiveInfo(true, R.drawable.ic_bold_font_active,  R.drawable.ic_bold_font)),
+    BoldFont(
+        iconResId = R.drawable.ic_bold_font,
+        titleResId = R.string.bold_font,
+        iconActiveInfo = IconActiveInfo(
+            true,
+            R.drawable.ic_bold_font_active,
+            R.drawable.ic_bold_font
+        )
+    ),
     IncreaseFont(iconResId = R.drawable.ic_font_increase, titleResId = R.string.font_size_increase),
     DecreaseFont(iconResId = R.drawable.ic_font_decrease, titleResId = R.string.font_size_decrease),
     FullScreen(iconResId = R.drawable.icon_fullscreen, titleResId = R.string.fullscreen),
@@ -31,7 +51,15 @@ enum class ToolbarAction(
     CloseTab(iconResId = R.drawable.icon_close, titleResId = R.string.close_tab),
     InputUrl(iconResId = R.drawable.ic_input_url, titleResId = R.string.input_url),
     NewTab(iconResId = R.drawable.icon_plus, titleResId = R.string.open_new_tab),
-    Desktop(iconResId = R.drawable.icon_desktop, titleResId = R.string.desktop_mode, iconActiveInfo = IconActiveInfo(true,  R.drawable.icon_desktop_activate,  R.drawable.icon_desktop)),
+    Desktop(
+        iconResId = R.drawable.icon_desktop,
+        titleResId = R.string.desktop_mode,
+        iconActiveInfo = IconActiveInfo(
+            true,
+            R.drawable.icon_desktop_activate,
+            R.drawable.icon_desktop
+        )
+    ),
     TOC(iconResId = R.drawable.ic_toc, titleResId = R.string.title_in_toc, isAddable = false),
     Search(iconResId = R.drawable.icon_search, titleResId = R.string.setting_title_search),
     DuplicateTab(iconResId = R.drawable.ic_copy, titleResId = R.string.duplicate_tab);
@@ -39,15 +67,15 @@ enum class ToolbarAction(
     companion object {
         fun fromOrdinal(value: Int) = values().first { it.ordinal == value }
         val defaultActions: List<ToolbarAction> = listOf(
-                Title,
-                Bookmark,
-                TabCount,
-                Back,
-                Refresh,
-                Touch,
-                Font,
-                ReaderMode,
-                Settings,
+            Title,
+            Bookmark,
+            TabCount,
+            NewTab,
+            Back,
+            Refresh,
+            Touch,
+            ReaderMode,
+            Settings,
         )
     }
 
@@ -62,7 +90,7 @@ enum class ToolbarAction(
 data class IconActiveInfo(
     val isActivable: Boolean = false,
     val activeResId: Int = 0,
-    val inactiveResId: Int =0,
+    val inactiveResId: Int = 0,
 )
 
 // a data class to wrap a state in it
