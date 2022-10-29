31cbd50ec94c4e952c04e6d7c2656649cd9ca0b1
Daniel Kao <daniel.kao@gmail.com>
2022-10-29 18:59:00 +0800

    fix: page offset height configuration UI is malfunctioned.

--- app/src/main/java/info/plateaukao/einkbro/preference/IntegerEditTextPreference.kt
+++ app/src/main/java/info/plateaukao/einkbro/preference/IntegerEditTextPreference.kt
@@ -4,11 +4,11 @@ import android.content.Context
 import android.util.AttributeSet
 import androidx.preference.EditTextPreference
 
-class IntegerEditTextPreference @JvmOverloads constructor(
-    context: Context,
-    attrs: AttributeSet? = null,
-    defStyle: Int = 0
-) : EditTextPreference(context, attrs, defStyle) {
+class IntegerEditTextPreference : EditTextPreference {
+    constructor(context: Context) : super(context)
+    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs)
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int, defStyleRes: Int) : super(context, attrs, defStyleAttr, defStyleRes)
 
     override fun getPersistedString(defaultReturnValue: String?): String {
         val defaultAsInt: Int = try {
