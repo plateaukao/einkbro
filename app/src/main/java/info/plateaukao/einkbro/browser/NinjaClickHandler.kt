0dad5282d526fffc2a9724936a6ab28949549e43
Daniel Kao <daniel.kao@gmail.com>
2022-12-02 03:17:39 +0800

    ui:convert png to webp; feat: anchor contextmenu dialog.

--- app/src/main/java/info/plateaukao/einkbro/browser/NinjaClickHandler.kt
+++ app/src/main/java/info/plateaukao/einkbro/browser/NinjaClickHandler.kt
@@ -2,11 +2,15 @@ package info.plateaukao.einkbro.browser
 
 import android.os.Handler
 import android.os.Message
+import android.view.MotionEvent
 import info.plateaukao.einkbro.view.NinjaWebView
 
 class NinjaClickHandler(private val webView: NinjaWebView) : Handler() {
+
+    var currentMotionEvent: MotionEvent? = null
+
     override fun handleMessage(message: Message) {
         super.handleMessage(message)
-        webView.browserController?.onLongPress(message)
+        webView.browserController?.onLongPress(message, currentMotionEvent)
     }
 }
