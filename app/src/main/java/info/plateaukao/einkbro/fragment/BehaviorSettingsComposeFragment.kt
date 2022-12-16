1f1913ab5a84a733031db33715300add49071bf1
Daniel Kao <daniel.kao@gmail.com>
2022-12-17 01:47:46 +0800

    ui: add summary string to behavior and toolbar configuration

--- app/src/main/java/info/plateaukao/einkbro/fragment/BehaviorSettingsComposeFragment.kt
+++ app/src/main/java/info/plateaukao/einkbro/fragment/BehaviorSettingsComposeFragment.kt
@@ -23,6 +23,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -31,6 +32,7 @@ import androidx.compose.ui.unit.sp
 import androidx.fragment.app.Fragment
 import info.plateaukao.einkbro.preference.ConfigManager
 import info.plateaukao.einkbro.R
+import info.plateaukao.einkbro.unit.ViewUnit
 import info.plateaukao.einkbro.view.compose.MyTheme
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
@@ -65,42 +67,50 @@ class BehaviorSettingsComposeFragment : Fragment(), KoinComponent, FragmentTitle
         BooleanSettingItem(
             R.string.setting_title_saveTabs,
             R.drawable.icon_tab_plus,
-            config.shouldSaveTabs
+            config.shouldSaveTabs,
+            R.string.setting_summary_saveTabs,
         ) { config.shouldSaveTabs = !config.shouldSaveTabs },
         BooleanSettingItem(
             R.string.setting_title_background_loading,
             R.drawable.icon_tab_plus,
-            config.enableWebBkgndLoad
+            config.enableWebBkgndLoad,
+            R.string.setting_summary_background_loading,
         ) { config.enableWebBkgndLoad = !config.enableWebBkgndLoad },
         BooleanSettingItem(
             R.string.setting_title_trim_input_url,
             R.drawable.icon_edit,
-            config.shouldTrimInputUrl
+            config.shouldTrimInputUrl,
+            R.string.setting_summary_trim_input_url,
         ) { config.shouldTrimInputUrl = !config.shouldTrimInputUrl },
         BooleanSettingItem(
             R.string.setting_title_prune_query_parameter,
             R.drawable.ic_filter,
-            config.shouldPruneQueryParameters
+            config.shouldPruneQueryParameters,
+            R.string.setting_summary_prune_query_parameter,
         ) { config.shouldPruneQueryParameters = !config.shouldPruneQueryParameters },
         BooleanSettingItem(
             R.string.setting_title_screen_awake,
             R.drawable.ic_eye,
-            config.keepAwake
+            config.keepAwake,
+            R.string.setting_summary_screen_awake,
         ) { config.keepAwake = !config.keepAwake },
         BooleanSettingItem(
             R.string.setting_title_confirm_tab_close,
             R.drawable.icon_close,
-            config.confirmTabClose
+            config.confirmTabClose,
+            R.string.setting_summary_confirm_tab_close,
         ) { config.confirmTabClose = !config.confirmTabClose },
         BooleanSettingItem(
             R.string.setting_title_vi_binding,
             R.drawable.ic_keyboard,
-            config.enableViBinding
+            config.enableViBinding,
+            R.string.setting_summary_vi_binding,
         ) { config.enableViBinding = !config.enableViBinding },
         BooleanSettingItem(
             R.string.setting_title_useUpDown,
             R.drawable.ic_page_down,
-            config.useUpDownPageTurn
+            config.useUpDownPageTurn,
+            R.string.setting_summary_useUpDownKey,
         ) { config.useUpDownPageTurn = !config.useUpDownPageTurn },
     )
 
@@ -109,30 +119,33 @@ class BehaviorSettingsComposeFragment : Fragment(), KoinComponent, FragmentTitle
 
 @Composable
 private fun BehaviorSettingsMainContent(settings: List<BooleanSettingItem>) {
+    val context = LocalContext.current
+    val showSummary = ViewUnit.isWideLayout(context)
     LazyVerticalGrid(
         modifier = Modifier
             .wrapContentHeight()
             .padding(10.dp),
         verticalArrangement = Arrangement.spacedBy(7.dp),
         horizontalArrangement = Arrangement.spacedBy(7.dp),
-        columns = GridCells.Fixed(1),
+        columns = GridCells.Fixed(2),
     ) {
         settings.forEach { setting ->
-            item { SettingItem(setting) }
+            item { SettingItem(setting, showSummary) }
         }
     }
 }
 
 @Composable
-fun SettingItem(setting: BooleanSettingItem) {
+fun SettingItem(setting: BooleanSettingItem, showSummary: Boolean = false) {
     val interactionSource = remember { MutableInteractionSource() }
     val pressed by interactionSource.collectIsPressedAsState()
     val checked = remember { mutableStateOf(setting.booleanPreference) }
     val borderWidth = if (checked.value || pressed) 3.dp else 1.dp
+    val height = if (showSummary) 80.dp else 70.dp
     Row(
         modifier = Modifier
             .width(IntrinsicSize.Max)
-            .height(80.dp)
+            .height(height)
             .border(borderWidth, MaterialTheme.colors.onBackground, RoundedCornerShape(7.dp))
             .clickable(
                 indication = null,
@@ -155,12 +168,27 @@ fun SettingItem(setting: BooleanSettingItem) {
                 .width(6.dp)
                 .fillMaxHeight()
         )
-        Text(
-            modifier = Modifier.wrapContentWidth(),
-            text = stringResource(id = setting.titleResId),
-            fontSize = 16.sp,
-            color = MaterialTheme.colors.onBackground
-        )
+        Column {
+            Text(
+                modifier = Modifier.wrapContentWidth(),
+                text = stringResource(id = setting.titleResId),
+                fontSize = 16.sp,
+                color = MaterialTheme.colors.onBackground
+            )
+            if (showSummary) {
+                Spacer(
+                    modifier = Modifier
+                        .height(5.dp)
+                        .fillMaxWidth()
+                )
+                Text(
+                    modifier = Modifier.wrapContentWidth(),
+                    text = stringResource(id = setting.summaryResId),
+                    fontSize = 12.sp,
+                    color = MaterialTheme.colors.onBackground
+                )
+            }
+        }
     }
 }
 
@@ -169,6 +197,7 @@ class BooleanSettingItem(
     override val titleResId: Int,
     override val iconId: Int,
     val booleanPreference: Boolean,
+    val summaryResId: Int = 0,
     val onClick: () -> Unit,
 ) : SettingItemInterface
 
