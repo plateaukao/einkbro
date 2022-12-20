23c5fc36cf3a8c92b797fd51b4cd1704f93858bc
Daniel Kao <daniel.kao@gmail.com>
2022-12-21 02:34:32 +0800

    refactor: re-write first setting page

--- app/src/main/java/info/plateaukao/einkbro/fragment/SettingsComposeFragment.kt
+++ app/src/main/java/info/plateaukao/einkbro/fragment/SettingsComposeFragment.kt
@@ -1,40 +1,16 @@
 package info.plateaukao.einkbro.fragment
 
-import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.foundation.interaction.collectIsPressedAsState
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.GridItemSpan
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.grid.itemsIndexed
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Icon
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
-import info.plateaukao.einkbro.BuildConfig
 import info.plateaukao.einkbro.preference.ConfigManager
 import info.plateaukao.einkbro.R
 import info.plateaukao.einkbro.view.compose.MyTheme
+import info.plateaukao.einkbro.view.dialog.DialogManager
 import info.plateaukao.einkbro.view.dialog.PrinterDocumentPaperSizeDialog
 import info.plateaukao.einkbro.view.dialog.TextInputDialog
 import kotlinx.coroutines.launch
@@ -43,6 +19,7 @@ import org.koin.core.component.inject
 
 class SettingsComposeFragment : Fragment(), KoinComponent {
     private val config: ConfigManager by inject()
+    private val dialogManager: DialogManager by lazy { DialogManager(requireActivity()) }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -52,32 +29,52 @@ class SettingsComposeFragment : Fragment(), KoinComponent {
         val composeView = ComposeView(requireContext())
         composeView.setContent {
             MyTheme {
-                SettingsMainContent(
-                    FirstLayerSettingItem.values().toList(),
-                    onItemClick = { handleSettingItem(it) },
-                )
+                SettingsMainContent(mainSettings, dialogManager, {}, 2)
             }
         }
         return composeView
     }
 
-    private fun handleSettingItem(setting: FirstLayerSettingItem) {
-        when (setting) {
-            FirstLayerSettingItem.Ui -> showFragment(createUiSettingFragment())
-            FirstLayerSettingItem.Toolbar -> showFragment(createToolbarSettingFragment())
-            FirstLayerSettingItem.Behavior -> showFragment(createBehaviorSettingFragment())
-            FirstLayerSettingItem.Font -> showFragment(FontSettingsFragment())
-            FirstLayerSettingItem.Gesture -> showFragment(FragmentSettingsGesture())
-            FirstLayerSettingItem.Backup -> showFragment(DataSettingsFragment())
-            FirstLayerSettingItem.PdfSize -> PrinterDocumentPaperSizeDialog(requireContext()).show()
-            FirstLayerSettingItem.StartControl -> showFragment(StartSettingsFragment())
-            FirstLayerSettingItem.ClearControl -> showFragment(ClearDataFragment())
-            FirstLayerSettingItem.Search -> showFragment(SearchSettingsFragment())
-            FirstLayerSettingItem.UserAgent -> lifecycleScope.launch { updateUserAgent() }
-            FirstLayerSettingItem.Homepage -> lifecycleScope.launch() { updateHomepage() }
-            FirstLayerSettingItem.About -> showFragment(createAboutFragment())
-        }
-    }
+    private val mainSettings = listOf(
+        ActionSettingItem(R.string.setting_title_ui, R.drawable.ic_phone) { showFragment(createUiSettingFragment()) },
+        ActionSettingItem(R.string.setting_title_toolbar, R.drawable.ic_toolbar) {
+            showFragment(createToolbarSettingFragment())
+        },
+        ActionSettingItem(R.string.setting_title_behavior, R.drawable.icon_ui) {
+            showFragment(createBehaviorSettingFragment())
+        },
+        ActionSettingItem(R.string.setting_title_font, R.drawable.icon_size) { showFragment(FontSettingsFragment()) },
+        ActionSettingItem(R.string.setting_gestures, R.drawable.gesture_tap) {
+            showFragment(FragmentSettingsGesture())
+        },
+        ActionSettingItem(R.string.setting_title_data, R.drawable.icon_backup) { showFragment(DataSettingsFragment()) },
+        ActionSettingItem(
+            R.string.setting_title_pdf_paper_size,
+            R.drawable.ic_pdf
+        ) { PrinterDocumentPaperSizeDialog(requireContext()).show() },
+        ActionSettingItem(
+            R.string.setting_title_start_control,
+            R.drawable.icon_earth
+        ) { showFragment(StartSettingsFragment()) },
+        ActionSettingItem(
+            R.string.setting_title_clear_control,
+            R.drawable.icon_delete
+        ) { showFragment(ClearDataFragment()) },
+        ActionSettingItem(R.string.setting_title_search, R.drawable.icon_search) {
+            showFragment(SearchSettingsFragment())
+        },
+        ActionSettingItem(
+            R.string.setting_title_userAgent,
+            R.drawable.icon_useragent
+        ) { lifecycleScope.launch { updateUserAgent() } },
+        ActionSettingItem(
+            R.string.setting_title_edit_homepage,
+            R.drawable.icon_edit
+        ) { lifecycleScope.launch() { updateHomepage() } },
+        VersionSettingItem(R.string.title_about, R.drawable.icon_info) {
+            showFragment(createAboutFragment())
+        },
+    )
 
     private fun showFragment(fragment: Fragment) {
         parentFragmentManager
@@ -242,7 +239,6 @@ class SettingsComposeFragment : Fragment(), KoinComponent {
         R.string.setting_title_toolbar, toolbarSettingItems
     )
 
-
     private val toolbarSettingItems = listOf(
         BooleanSettingItem(
             R.string.setting_title_toolbar_top,
@@ -271,76 +267,6 @@ class SettingsComposeFragment : Fragment(), KoinComponent {
     )
 
     private fun createAboutFragment() = UISettingsComposeFragment(
-        R.string.title_about, AboutSettingItem.values().toList()
+        R.string.title_about, LinkSettingItem.values().toList()
     )
-}
-
-@Composable
-fun <T : SettingItemInterface> SettingsMainContent(
-    settings: List<T>,
-    onItemClick: (T) -> Unit,
-) {
-    LazyVerticalGrid(
-        modifier = Modifier
-            .wrapContentHeight()
-            .padding(10.dp),
-        verticalArrangement = Arrangement.spacedBy(7.dp),
-        horizontalArrangement = Arrangement.spacedBy(7.dp),
-        columns = GridCells.Fixed(2),
-    ) {
-        settings.forEach { setting ->
-            if (setting == FirstLayerSettingItem.About) {
-                item(span = { GridItemSpan(2) }) {
-                    VersionItem(
-                        setting as FirstLayerSettingItem,
-                        onItemClick = { onItemClick(setting) })
-                }
-            } else {
-                item { SettingItem(setting, onItemClick) }
-            }
-        }
-    }
-}
-
-
-enum class FirstLayerSettingItem(
-    override val titleResId: Int,
-    override val iconId: Int,
-    override val summaryResId: Int = 0,
-) : SettingItemInterface {
-    Ui(R.string.setting_title_ui, R.drawable.ic_phone),
-    Toolbar(R.string.setting_title_toolbar, R.drawable.ic_toolbar),
-    Behavior(R.string.setting_title_behavior, R.drawable.icon_ui),
-    Font(R.string.setting_title_font, R.drawable.icon_size),
-    Gesture(R.string.setting_gestures, R.drawable.gesture_tap),
-    Backup(R.string.setting_title_data, R.drawable.icon_backup),
-    PdfSize(R.string.setting_title_pdf_paper_size, R.drawable.ic_pdf),
-    StartControl(R.string.setting_title_start_control, R.drawable.icon_earth),
-    ClearControl(R.string.setting_title_clear_control, R.drawable.icon_delete),
-    Search(R.string.setting_title_search, R.drawable.icon_search),
-    UserAgent(R.string.setting_title_userAgent, R.drawable.icon_useragent),
-    Homepage(R.string.setting_title_edit_homepage, R.drawable.icon_edit),
-    About(R.string.menu_other_info, R.drawable.icon_info),
-}
-
-@Preview
-@Composable
-fun PreviewSettingsMainContent() {
-    MyTheme {
-        SettingsMainContent(
-            FirstLayerSettingItem.values().toList(),
-            onItemClick = {}
-        )
-    }
-}
-
-@Preview(uiMode = UI_MODE_NIGHT_YES)
-@Composable
-fun PreviewNightSettingsMainContent() {
-    MyTheme {
-        SettingsMainContent(
-            FirstLayerSettingItem.values().toList(),
-            onItemClick = {},
-        )
-    }
 }
