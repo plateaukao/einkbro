081f343dde928f79fa66a4bbf40e14e603a48e1a
Daniel Kao <daniel.kao@gmail.com>
2023-04-15 13:43:29 +0800

    feat: integrate chatgpt API!!

--- app/src/main/java/info/plateaukao/einkbro/viewmodel/PocketViewModel.kt
+++ app/src/main/java/info/plateaukao/einkbro/viewmodel/PocketViewModel.kt
@@ -24,8 +24,10 @@ class PocketViewModel : KoinComponent, ViewModel() {
         if (IntentUnit.isPocketInstalled(context)) {
             return try {
                 val intent = Intent().apply {
-                    setClassName("com.ideashower.readitlater.pro",
-                        "com.ideashower.readitlater.activity.AddActivity")
+                    setClassName(
+                        "com.ideashower.readitlater.pro",
+                        "com.ideashower.readitlater.activity.AddActivity"
+                    )
                     action = Intent.ACTION_SEND
                     type = "text/plain"
                     putExtra(Intent.EXTRA_TEXT, url)
@@ -45,6 +47,7 @@ class PocketViewModel : KoinComponent, ViewModel() {
     fun isPocketLoggedIn(): Boolean {
         return configManager.pocketAccessToken.isNotBlank()
     }
+
     suspend fun getAuthUrl(): String {
         return suspendCancellableCoroutine { continuation ->
             pocketNetwork.getRequestToken { requestToken ->
