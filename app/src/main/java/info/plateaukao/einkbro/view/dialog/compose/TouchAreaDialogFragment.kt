935d2709c5a583d8d43b1b1ea9b59d4914f83d6a
Daniel Kao <daniel.kao@gmail.com>
2023-04-16 12:37:24 +0800

    ui: refine UI for touch area config

--- app/src/main/java/info/plateaukao/einkbro/view/dialog/compose/TouchAreaDialogFragment.kt
+++ app/src/main/java/info/plateaukao/einkbro/view/dialog/compose/TouchAreaDialogFragment.kt
@@ -3,9 +3,18 @@ package info.plateaukao.einkbro.view.dialog.compose
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Switch
 import androidx.compose.material.Text
 import androidx.compose.material.TextButton
 import androidx.compose.runtime.Composable
@@ -21,15 +30,19 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import info.plateaukao.einkbro.R
-import info.plateaukao.einkbro.view.compose.MyTheme
 import info.plateaukao.einkbro.preference.TouchAreaType
-import info.plateaukao.einkbro.preference.TouchAreaType.*
+import info.plateaukao.einkbro.preference.TouchAreaType.BottomLeftRight
+import info.plateaukao.einkbro.preference.TouchAreaType.Left
+import info.plateaukao.einkbro.preference.TouchAreaType.MiddleLeftRight
+import info.plateaukao.einkbro.preference.TouchAreaType.Right
 import info.plateaukao.einkbro.preference.toggle
+import info.plateaukao.einkbro.view.compose.MyTheme
 import org.koin.core.component.KoinComponent
 
 class TouchAreaDialogFragment : ComposeDialogFragment(), KoinComponent {
     override fun setupComposeView() = composeView.setContent {
-        val touchAreaType  = remember { mutableStateOf(config.touchAreaType) }
+        val touchAreaType = remember { mutableStateOf(config.touchAreaType) }
+        val enableTurn = remember { mutableStateOf(config.enableTouchTurn) }
         MyTheme {
             TouchAreaContent(
                 touchAreaType = touchAreaType.value,
@@ -37,14 +50,18 @@ class TouchAreaDialogFragment : ComposeDialogFragment(), KoinComponent {
                     config.touchAreaType = type
                     touchAreaType.value = type
                 },
-                onAction = { config.enableTouchTurn = true; dismiss() },
-                offAction = { config.enableTouchTurn = false ; dismiss() },
+                enableTurn = enableTurn.value,
+                onToggle = {
+                    config::enableTouchTurn.toggle()
+                    enableTurn.value = config.enableTouchTurn
+                },
                 showHint = config.touchAreaHint,
                 hideTouchWhenType = config.hideTouchAreaWhenInput,
                 switchTouchArea = config.switchTouchAreaAction,
                 onShowHintClick = { config::touchAreaHint.toggle() },
                 onHideWhenTypeClick = { config::hideTouchAreaWhenInput.toggle() },
                 onSwitchAreaClick = { config::switchTouchAreaAction.toggle() },
+                onCloseClick = { dismiss() },
             )
         }
     }
@@ -54,14 +71,15 @@ class TouchAreaDialogFragment : ComposeDialogFragment(), KoinComponent {
 fun TouchAreaContent(
     touchAreaType: TouchAreaType,
     onTouchTypeClick: (TouchAreaType) -> Unit,
-    onAction: () -> Unit,
-    offAction: () -> Unit,
+    enableTurn: Boolean = true,
+    onToggle: () -> Unit = {},
     showHint: Boolean = true,
     hideTouchWhenType: Boolean = true,
     switchTouchArea: Boolean = false,
     onShowHintClick: () -> Unit = {},
     onHideWhenTypeClick: () -> Unit = {},
     onSwitchAreaClick: () -> Unit = {},
+    onCloseClick: () -> Unit = {},
 ) {
     Column(Modifier.width(300.dp)) {
         Text(
@@ -74,39 +92,84 @@ fun TouchAreaContent(
             textAlign = TextAlign.Center,
         )
         Row(Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceAround) {
-            TouchAreaItem(state = touchAreaType == Left, titleResId = R.string.touch_left_side, iconResId = R.drawable.ic_touch_left) { onTouchTypeClick(Left) }
-            TouchAreaItem(state = touchAreaType == Right, titleResId = R.string.touch_right_side, iconResId = R.drawable.ic_touch_right) { onTouchTypeClick(Right) }
-        }
-        Row(Modifier.fillMaxWidth( ), horizontalArrangement = Arrangement.SpaceAround) {
-            TouchAreaItem(state = touchAreaType == MiddleLeftRight, titleResId = R.string.middle, iconResId = R.drawable.ic_touch_middle_left_right) { onTouchTypeClick(MiddleLeftRight) }
-            TouchAreaItem(state = touchAreaType == BottomLeftRight, titleResId = R.string.bottom, iconResId = R.drawable.ic_touch_left_right) { onTouchTypeClick(BottomLeftRight) }
+            TouchAreaItem(
+                state = touchAreaType == Left,
+                titleResId = R.string.touch_left_side,
+                iconResId = R.drawable.ic_touch_left
+            ) { onTouchTypeClick(Left) }
+            TouchAreaItem(
+                state = touchAreaType == Right,
+                titleResId = R.string.touch_right_side,
+                iconResId = R.drawable.ic_touch_right
+            ) { onTouchTypeClick(Right) }
+            TouchAreaItem(
+                state = touchAreaType == MiddleLeftRight,
+                titleResId = R.string.middle,
+                iconResId = R.drawable.ic_touch_middle_left_right
+            ) { onTouchTypeClick(MiddleLeftRight) }
+            TouchAreaItem(
+                state = touchAreaType == BottomLeftRight,
+                titleResId = R.string.bottom,
+                iconResId = R.drawable.ic_touch_left_right
+            ) { onTouchTypeClick(BottomLeftRight) }
         }
 
         Spacer(modifier = Modifier.height(10.dp))
 
-        ToggleItem(state = showHint, titleResId = R.string.show_touch_area_hint, iconResId = -1, onClicked = { onShowHintClick() } )
-        ToggleItem(state = hideTouchWhenType, titleResId = R.string.hie_touch_area_when_input, iconResId = -1, onClicked = { onHideWhenTypeClick() } )
-        ToggleItem(state = switchTouchArea, titleResId = R.string.switch_touch_area_action, iconResId = -1, onClicked ={ onSwitchAreaClick() } )
+        ToggleItem(
+            state = showHint,
+            titleResId = R.string.show_touch_area_hint,
+            iconResId = -1,
+            onClicked = { onShowHintClick() })
+        ToggleItem(
+            state = hideTouchWhenType,
+            titleResId = R.string.hie_touch_area_when_input,
+            iconResId = -1,
+            onClicked = { onHideWhenTypeClick() })
+        ToggleItem(
+            state = switchTouchArea,
+            titleResId = R.string.switch_touch_area_action,
+            iconResId = -1,
+            onClicked = { onSwitchAreaClick() })
 
         Spacer(modifier = Modifier.height(10.dp))
+
+        Row(Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.End) {
+            Text(
+                modifier = Modifier
+                    .padding(10.dp)
+                    .weight(1f),
+                text = stringResource(R.string.switch_touch_area_action),
+                color = MaterialTheme.colors.onBackground,
+                textAlign = TextAlign.Start,
+            )
+            Switch(
+                modifier = Modifier
+                    .wrapContentHeight(),
+                checked = enableTurn,
+                onCheckedChange = {
+                    onToggle()
+                },
+            )
+        }
+
         HorizontalSeparator()
 
-        Row(modifier = Modifier
-            .fillMaxWidth()
-            .wrapContentHeight(),
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentHeight(),
             verticalAlignment = Alignment.CenterVertically,
             horizontalArrangement = Arrangement.End
         ) {
             TextButton(
-                modifier = Modifier.wrapContentWidth(),
-                onClick = offAction) {
-                Text(stringResource(id = R.string.turn_off), color = MaterialTheme.colors.onBackground)
-            }
-            VerticalSeparator()
-            TextButton(
-                modifier = Modifier.wrapContentWidth(),
-                onClick = onAction) {
-                Text(stringResource(id = R.string.turn_on), color = MaterialTheme.colors.onBackground)
+                modifier = Modifier.weight(1f),
+                onClick = onCloseClick
+            ) {
+                Text(
+                    stringResource(id = android.R.string.ok),
+                    color = MaterialTheme.colors.onBackground
+                )
             }
         }
     }
@@ -121,25 +184,28 @@ fun TouchAreaItem(
 ) {
     val interactionSource = remember { MutableInteractionSource() }
     val borderWidth = if (state) 4.dp else -1.dp
-    Column (
+    Column(
         modifier = Modifier
-            .padding(15.dp)
+            .padding(5.dp)
             .clickable(
                 indication = null,
-                interactionSource = interactionSource) {
+                interactionSource = interactionSource
+            ) {
                 onClicked()
             },
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center,
-    ){
+    ) {
         Icon(
             painter = painterResource(id = iconResId),
             contentDescription = null,
-            modifier = Modifier .border(borderWidth, MaterialTheme.colors.onBackground),
+            modifier = Modifier
+                .height(80.dp)
+                .border(borderWidth, MaterialTheme.colors.onBackground),
             tint = MaterialTheme.colors.onBackground
         )
         Text(
-            modifier = Modifier.padding(top = 10.dp),
+            modifier = Modifier.padding(top = 6.dp),
             text = stringResource(id = titleResId),
             fontSize = 13.sp,
             color = MaterialTheme.colors.onBackground,
@@ -149,7 +215,7 @@ fun TouchAreaItem(
 }
 
 @Preview(
-    name  = "default",
+    name = "default",
     showSystemUi = true,
     showBackground = true, device = "spec:shape=Normal,width=1080,height=2340,unit=px,dpi=440",
 )
@@ -158,9 +224,7 @@ fun PreviewTouchAreaContent() {
     MaterialTheme {
         TouchAreaContent(
             touchAreaType = Left,
-            onTouchTypeClick = { },
-            onAction = {},
-            offAction = {}
+            onTouchTypeClick = {},
         )
     }
 }
