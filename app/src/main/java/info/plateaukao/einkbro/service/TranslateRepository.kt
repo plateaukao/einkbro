85516380b776e0c778342125772c7cf6cd4b6038
Daniel Kao <daniel.kao@gmail.com>
2023-04-17 21:48:11 +0800

    refactor: refactor suspendCoroutine to normal okhttp call

--- app/src/main/java/info/plateaukao/einkbro/service/TranslateRepository.kt
+++ app/src/main/java/info/plateaukao/einkbro/service/TranslateRepository.kt
@@ -1,26 +1,21 @@
 package info.plateaukao.einkbro.service
 
-import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.suspendCancellableCoroutine
-import okhttp3.Call
-import okhttp3.Callback
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.withContext
 import okhttp3.HttpUrl
 import okhttp3.OkHttpClient
 import okhttp3.Request
-import okhttp3.Response
-import okio.IOException
 import org.jsoup.Jsoup
 
 class TranslateRepository {
     private val client = OkHttpClient()
 
-    @OptIn(ExperimentalCoroutinesApi::class)
     suspend fun gTranslate(
         text: String,
         targetLanguage: String = "en",
         sourceLanguage: String = "auto",
     ): String? {
-        return suspendCancellableCoroutine { continuation ->
+        return withContext(IO) {
             val url = HttpUrl.Builder()
                 .scheme("https")
                 .host("translate.google.com")
@@ -30,36 +25,15 @@ class TranslateRepository {
                 .addQueryParameter("q", text)
                 .build()
 
-            val request = Request.Builder()
-                .url(url)
-                .build()
+            val request = Request.Builder().url(url).build()
 
-            client.newCall(request).enqueue(object : Callback {
-                override fun onFailure(call: Call, e: IOException) {
-                    if (continuation.isActive) {
-                        continuation.resume(null) {}
-                    }
-                }
+            client.newCall(request).execute().use { response ->
+                val body = response.body?.string() ?: return@use null
 
-                override fun onResponse(call: Call, response: Response) {
-                    if (continuation.isActive) {
-                        val body = response.body?.string()
-                        if (body != null) {
-                            Jsoup.parse(body)
-                                .body()
-                                .getElementsByClass("result-container")
-                                .first()?.text()?.let {
-                                    continuation.resume(it) {}
-                                } ?: continuation.resume(null) {}
-                        } else {
-                            continuation.resume(null) {}
-                        }
-                    }
-                }
-            })
-
-            continuation.invokeOnCancellation {
-                client.dispatcher.executorService.shutdownNow()
+                Jsoup.parse(body)
+                    .body()
+                    .getElementsByClass("result-container")
+                    .first()?.text()
             }
         }
     }
