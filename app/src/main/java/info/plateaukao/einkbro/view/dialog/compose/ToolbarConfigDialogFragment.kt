7ce10f6cd0d9a35a9424cf669f5fc7c9ae0d50d6
Daniel Kao <daniel.kao@gmail.com>
2023-04-20 00:09:57 +0800

    ver: update to 10.3.0

--- app/src/main/java/info/plateaukao/einkbro/view/dialog/compose/ToolbarConfigDialogFragment.kt
+++ app/src/main/java/info/plateaukao/einkbro/view/dialog/compose/ToolbarConfigDialogFragment.kt
@@ -2,12 +2,28 @@ package info.plateaukao.einkbro.view.dialog.compose
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.*
-import androidx.compose.runtime.*
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.material.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -17,9 +33,12 @@ import androidx.compose.ui.unit.dp
 import info.plateaukao.einkbro.R
 import info.plateaukao.einkbro.view.compose.MyTheme
 import info.plateaukao.einkbro.view.toolbaricons.ToolbarAction
-import org.burnoutcrew.reorderable.*
+import org.burnoutcrew.reorderable.ReorderableItem
+import org.burnoutcrew.reorderable.detectReorder
+import org.burnoutcrew.reorderable.rememberReorderableLazyListState
+import org.burnoutcrew.reorderable.reorderable
 
-class ToolbarConfigDialogFragment: ComposeDialogFragment(){
+class ToolbarConfigDialogFragment : ComposeDialogFragment() {
     override fun setupComposeView() {
         val actionInfoList = getCurrentActionList()
         composeView.setContent {
@@ -54,14 +73,16 @@ class ToolbarConfigDialogFragment: ComposeDialogFragment(){
                             rememberList = actionInfos
                         },
                         onItemMoved = { from, to ->
-                            rememberList = rememberList.toMutableList().apply { add(to, removeAt(from)) }
+                            rememberList =
+                                rememberList.toMutableList().apply { add(to, removeAt(from)) }
                         }
                     )
                     HorizontalSeparator()
                     DialogButtonBar(
                         dismissAction = { dialog?.dismiss() },
                         okAction = {
-                            config.toolbarActions = rememberList.filter { it.isOn }.map { it.toolbarAction }
+                            config.toolbarActions =
+                                rememberList.filter { it.isOn }.map { it.toolbarAction }
                         }
                     )
                 }
@@ -71,16 +92,21 @@ class ToolbarConfigDialogFragment: ComposeDialogFragment(){
 
     private fun getCurrentActionList(): List<ToolbarActionItemInfo> =
         config.toolbarActions.map { ToolbarActionItemInfo(it, true) } +
-            // need to filter only addable actions here
-        ToolbarAction.values().filter { it.isAddable }.filterNot { config.toolbarActions.contains(it) }.map { ToolbarActionItemInfo(it, false) }
+                ToolbarAction.values()
+                    // need to filter only addable actions here
+                    .filter { it.isAddable }
+                    .filterNot { config.toolbarActions.contains(it) }
+                    // hide papago action if papago api key is not set
+                    .filterNot { config.papagoApiSecret.isBlank() && it == ToolbarAction.PapagoByParagraph }
+                    .map { ToolbarActionItemInfo(it, false) }
 }
 
 @Composable
 private fun ToolbarList(
     modifier: Modifier,
     infos: List<ToolbarActionItemInfo>,
-    onItemClicked: (ToolbarAction)->Unit,
-    onItemMoved: (Int, Int)->Unit
+    onItemClicked: (ToolbarAction) -> Unit,
+    onItemMoved: (Int, Int) -> Unit
 ) {
     val state = rememberReorderableLazyListState(onMove = { from, to ->
         onItemMoved(from.index, to.index)
@@ -98,7 +124,11 @@ private fun ToolbarList(
                 val borderWidth = if (isDragging) 1.5.dp else -1.dp
                 Column(
                     modifier = Modifier
-                        .border(borderWidth, MaterialTheme.colors.onBackground, RoundedCornerShape(3.dp))
+                        .border(
+                            borderWidth,
+                            MaterialTheme.colors.onBackground,
+                            RoundedCornerShape(3.dp)
+                        )
                         .background(MaterialTheme.colors.background)
                 ) {
                     ToolbarToggleItem(
@@ -115,14 +145,14 @@ private fun ToolbarList(
 @Composable
 fun ToolbarToggleItem(
     info: ToolbarActionItemInfo,
-    onItemClicked: (ToolbarAction)->Unit,
+    onItemClicked: (ToolbarAction) -> Unit,
     dragModifier: Modifier,
 ) {
     Row(
         verticalAlignment = Alignment.CenterVertically,
     ) {
         val shouldEnableCheckClick = info.toolbarAction != ToolbarAction.Settings
-        Surface(modifier = Modifier.weight(1F), color = MaterialTheme.colors.background){
+        Surface(modifier = Modifier.weight(1F), color = MaterialTheme.colors.background) {
             ToggleItem(
                 state = info.isOn,
                 titleResId = info.toolbarAction.titleResId,
@@ -134,29 +164,39 @@ fun ToolbarToggleItem(
         }
         Icon(
             painter = painterResource(id = R.drawable.ic_drag), contentDescription = null,
-            modifier = dragModifier.padding(horizontal = 8.dp).fillMaxHeight(),
+            modifier = dragModifier
+                .padding(horizontal = 8.dp)
+                .fillMaxHeight(),
             tint = MaterialTheme.colors.onBackground
         )
     }
 }
 
 @Composable
-fun DialogButtonBar(dismissAction: ()->Unit, okAction: ()->Unit) {
-    Row(modifier = Modifier
-        .width(IntrinsicSize.Max)
-        .wrapContentHeight(),
+fun DialogButtonBar(dismissAction: () -> Unit, okAction: () -> Unit) {
+    Row(
+        modifier = Modifier
+            .width(IntrinsicSize.Max)
+            .wrapContentHeight(),
         verticalAlignment = Alignment.CenterVertically
     ) {
         TextButton(
             modifier = Modifier.wrapContentWidth(),
-            onClick = dismissAction) {
-            Text(stringResource(id = android.R.string.cancel), color = MaterialTheme.colors.onBackground)
+            onClick = dismissAction
+        ) {
+            Text(
+                stringResource(id = android.R.string.cancel),
+                color = MaterialTheme.colors.onBackground
+            )
         }
         VerticalSeparator()
         TextButton(
             modifier = Modifier.wrapContentWidth(),
             onClick = { dismissAction(); okAction() }) {
-            Text(stringResource(id = android.R.string.ok), color = MaterialTheme.colors.onBackground)
+            Text(
+                stringResource(id = android.R.string.ok),
+                color = MaterialTheme.colors.onBackground
+            )
         }
     }
 }
@@ -167,7 +207,7 @@ class ToolbarActionItemInfo(val toolbarAction: ToolbarAction, var isOn: Boolean)
 @Composable
 private fun previewToolBar() {
     MyTheme {
-        Column (horizontalAlignment = Alignment.End){
+        Column(horizontalAlignment = Alignment.End) {
             ToolbarList(
                 Modifier
                     .heightIn(100.dp, 500.dp)
@@ -175,7 +215,7 @@ private fun previewToolBar() {
                     .padding(2.dp), // for round corner spaces
                 ToolbarAction.values().map { ToolbarActionItemInfo(it, true) },
                 {},
-                {_, _-> }
+                { _, _ -> }
             )
             DialogButtonBar({}, {})
         }
