7a18273147231b61adbca91585ec61c59b0364e2
Daniel Kao <daniel.kao@gmail.com>
2024-01-21 21:19:08 +0800

    fix:#320 remove bookmarks when typing input url.

--- app/src/main/java/info/plateaukao/einkbro/view/compose/AutoCompleteTextField.kt
+++ app/src/main/java/info/plateaukao/einkbro/view/compose/AutoCompleteTextField.kt
@@ -5,13 +5,29 @@ import android.app.Activity
 import android.content.Context
 import android.util.AttributeSet
 import android.view.KeyEvent.KEYCODE_ENTER
-import androidx.compose.foundation.*
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.*
-import androidx.compose.runtime.*
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
@@ -105,7 +121,7 @@ fun AutoCompleteTextField(
             it.title?.contains(text.value.text, ignoreCase = true) == true
                     || it.url.contains(text.value.text, ignoreCase = true)
         }
-        if (list.isNotEmpty()) list else recordList.value
+        list.ifEmpty { recordList.value }
     }
 
     Column(
