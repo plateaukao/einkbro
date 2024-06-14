4fb218d6d60c7d361170b13a5507ae62747c0b37
Daniel Kao <daniel.kao@gmail.com>
2024-06-14 23:46:19 +0800

    ver: to 11.10.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "info.plateaukao.einkbro"
         minSdk = 24
         targetSdk = 34
-        versionCode = 11_09_00
-        versionName = "11.9.0"
+        versionCode = 11_10_00
+        versionName = "11.10.0"
 
         buildConfigField("String", "builtDateTime", "\"${getCurrentTimestamp()}\"")
         buildConfigField("boolean", "showUpdateButton", showUpdateButton())
