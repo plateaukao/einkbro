c31930e1a4f31d97cda516c760352304351b95e3
wanderor <richardfang@msn.com>
2024-08-13 01:48:33 +0800

    Add toolbar actions to switch touch direction
    Originally, switching touch direction would require multiple steps. This CL adds 2 toolbar actions to allow fast switch in one step.
    
    The new 2 toolbar actions have identical functionality and different icons.

--- app/src/main/java/info/plateaukao/einkbro/activity/BrowserActivity.kt
+++ app/src/main/java/info/plateaukao/einkbro/activity/BrowserActivity.kt
@@ -1072,6 +1072,8 @@ open class BrowserActivity : FragmentActivity(), BrowserController {
 
     override fun toggleTouchTurnPageFeature() = config::enableTouchTurn.toggle()
 
+    override fun toggleSwitchTouchAreaAction() = config::switchTouchAreaAction.toggle()
+
     private fun updateTouchView() = composeToolbarViewController.updateIcons()
 
     // Methods
@@ -1605,6 +1607,10 @@ open class BrowserActivity : FragmentActivity(), BrowserController {
                 ConfigManager.K_ENABLE_TOUCH -> {
                     updateTouchView()
                     touchController.toggleTouchPageTurn(config.enableTouchTurn)
+                }
+
+                ConfigManager.K_TOUCH_AREA_ACTION_SWITCH -> {
+                    updateTouchView()
                 }
             }
         }
