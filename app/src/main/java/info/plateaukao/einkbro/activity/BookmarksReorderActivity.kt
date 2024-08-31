f60735ad8e672e7b44493af8ec0ed90327f5e4bf
Daniel Kao <daniel.kao@gmail.com>
2024-08-31 21:34:36 +0800

    ver: update library versions

--- app/src/main/java/info/plateaukao/einkbro/activity/BookmarksReorderActivity.kt
+++ app/src/main/java/info/plateaukao/einkbro/activity/BookmarksReorderActivity.kt
@@ -9,13 +9,17 @@ import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
@@ -28,11 +32,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.zIndex
 import androidx.lifecycle.lifecycleScope
 import info.plateaukao.einkbro.R
 import info.plateaukao.einkbro.database.Bookmark
@@ -153,12 +159,16 @@ fun BookmarksReorderContent(
                 reorderableLazyListState,
                 key = bookmark.id,
             ) { isDragging ->
-                val elevation by animateDpAsState(if (isDragging) 30.dp else 0.dp, label = "")
-
+                val borderWidth = if (isDragging) 1.5.dp else -1.dp
                 Row(
-                    modifier = Modifier.clickable {
-                        onBookmarkClick(bookmark)
-                    },
+                    modifier = Modifier
+                        .border(
+                            borderWidth,
+                            MaterialTheme.colors.onBackground,
+                            RoundedCornerShape(3.dp)
+                        )
+                        .background(MaterialTheme.colors.background)
+                        .clickable { onBookmarkClick(bookmark) },
                     verticalAlignment = Alignment.CenterVertically,
                 ) {
                     BookmarkItem(
