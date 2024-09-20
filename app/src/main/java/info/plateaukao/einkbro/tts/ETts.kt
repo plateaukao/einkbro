dd7cee7c3dcd7e0763cdee141291b79515ba96f4
Daniel Kao <daniel.kao@gmail.com>
2024-09-20 22:52:37 +0800

    refactor: tts implementation

--- app/src/main/java/info/plateaukao/einkbro/tts/ETts.kt
+++ app/src/main/java/info/plateaukao/einkbro/tts/ETts.kt
@@ -1,9 +1,7 @@
 package info.plateaukao.einkbro.tts
 
-import android.media.MediaPlayer
 import icu.xmc.edgettslib.entity.VoiceItem
 import info.plateaukao.einkbro.EinkBroApplication
-import kotlinx.serialization.json.Json
 import okhttp3.Headers.Companion.toHeaders
 import okhttp3.OkHttpClient
 import okhttp3.Request
@@ -18,41 +16,15 @@ import kotlin.coroutines.resume
 import kotlin.coroutines.suspendCoroutine
 
 // ported from https://github.com/9ikj/Edge-TTS-Lib/
-class ETts private constructor() {
-
-    companion object {
-        private var sInstance: ETts? = null
-            get() {
-                if (field == null) {
-                    field = ETts()
-                }
-                return field
-            }
-
-        fun getInstance(): ETts {
-            return sInstance!!
-        }
-
-    }
-
-    private val json = Json { ignoreUnknownKeys = true }
-
+class ETts {
     private var headers: HashMap<String, String> = HashMap<String, String>().apply {
-        put("Origin", UrlConstant.EDGE_ORIGIN)
+        put("Origin", EDGE_ORIGIN)
         put("Pragma", "no-cache")
         put("Cache-Control", "no-cache")
-        put("User-Agent", UrlConstant.EDGE_UA)
+        put("User-Agent", EDGE_UA)
     }
-    private var format = "audio-24khz-48kbitrate-mono-mp3"
-    private var findHeadHook = false
-    private var voicePitch = "+0Hz"
-    private var voiceRate = "+0%"
-    private var voiceVolume = "+0%"
-    private var storage = ""
-    private var mediaPlayer: MediaPlayer? = null
-    private var lastPlayMp3: File? = null
-
-    private var request: Request? = null
+    private val format = "audio-24khz-48kbitrate-mono-mp3"
+    private val storage = EinkBroApplication.instance.cacheDir.absolutePath
 
     private val okHttpClient by lazy {
         OkHttpClient.Builder()
@@ -62,72 +34,54 @@ class ETts private constructor() {
             .build()
     }
 
-    fun initialize() {
-        storage = EinkBroApplication.instance.cacheDir.absolutePath
-        mediaPlayer = MediaPlayer()
-        mediaPlayer?.setOnCompletionListener {
-            lastPlayMp3?.delete()
-        }
-    }
-
-    fun storage(storage: String): ETts {
-        this.storage = storage
-        return this
-    }
-
     suspend fun tts(voice: VoiceItem, speed: Int, content: String): File? =
         suspendCoroutine { continuation ->
-            val str = removeIncompatibleCharacters(content)
-            if (str.isNullOrBlank()) {
+            val processedContent = removeIncompatibleCharacters(content)
+            if (processedContent.isNullOrBlank()) {
                 continuation.resume(null)
             }
             val storageFolder = File(storage)
             if (!storageFolder.exists()) {
                 storageFolder.mkdirs()
             }
+
             val dateStr = dateToString(Date())
             val reqId = uuid()
+            var fileName = "$reqId.mp3"
             val audioFormat = mkAudioFormat(dateStr, format)
             val ssml = mkssml(
                 voice.locale,
                 voice.name,
-                content,
-                voicePitch,
+                processedContent,
+                "+0Hz",
                 "+${speed - 100}%",
-                voiceVolume
+                "+0%"
             )
             val ssmlHeadersPlusData = ssmlHeadersPlusData(reqId, dateStr, ssml)
-            var fileName = reqId
-            if (format == "audio-24khz-48kbitrate-mono-mp3") {
-                fileName += ".mp3"
-            } else if (format == "webm-24khz-16bit-mono-opus") {
-                fileName += ".opus"
-            }
+
             val storageFile = File(storage)
             if (!storageFile.exists()) {
                 storageFile.mkdirs()
             }
-            if (request == null) {
-                request = Request.Builder()
-                    .url(UrlConstant.EDGE_URL)
-                    .headers(headers.toHeaders())
-                    .build()
-            }
+
+            val request = Request.Builder()
+                .url(EDGE_URL)
+                .headers(headers.toHeaders())
+                .build()
 
             try {
-                val client =
-                    okHttpClient.newWebSocket(
-                        request!!,
-                        object : TTSWebSocketListener(storage, fileName, findHeadHook) {
-                            override fun onClosed(webSocket: WebSocket, code: Int, reason: String) {
-                                val file = File(storage, fileName)
-                                if (file.exists()) {
-                                    continuation.resume(file)
-                                } else {
-                                    continuation.resume(null)
-                                }
+                val client = okHttpClient.newWebSocket(
+                    request,
+                    object : TTSWebSocketListener(storage, fileName) {
+                        override fun onClosed(webSocket: WebSocket, code: Int, reason: String) {
+                            val file = File(storage, fileName)
+                            if (file.exists()) {
+                                continuation.resume(file)
+                            } else {
+                                continuation.resume(null)
                             }
-                        })
+                        }
+                    })
                 client.send(audioFormat)
                 client.send(ssmlHeadersPlusData)
             } catch (e: Throwable) {
@@ -137,7 +91,9 @@ class ETts private constructor() {
         }
 
     private fun dateToString(date: Date): String {
-        val sdf = SimpleDateFormat("EEE MMM dd yyyy HH:mm:ss 'GMT'Z (zzzz)", Locale.getDefault())
+        val sdf = SimpleDateFormat(
+            "EEE MMM dd yyyy HH:mm:ss 'GMT'Z (zzzz)", Locale.getDefault()
+        )
         return sdf.format(date)
     }
 
@@ -145,9 +101,9 @@ class ETts private constructor() {
         return UUID.randomUUID().toString().replace("-", "")
     }
 
-    private fun removeIncompatibleCharacters(input: String): String? {
+    private fun removeIncompatibleCharacters(input: String): String {
         if (input.isBlank()) {
-            return null
+            return ""
         }
         val output = StringBuilder()
         for (element in input) {
@@ -186,4 +142,14 @@ class ETts private constructor() {
                 "Content-Type:application/ssml+xml\r\n" +
                 "X-Timestamp:" + timestamp + "Z\r\n" +
                 "Path:ssml\r\n\r\n" + ssml
+
+    companion object {
+        const val EDGE_URL =
+            "wss://speech.platform.bing.com/consumer/speech/synthesize/readaloud/edge/v1?TrustedClientToken=6A5AA1D4EAFF4E9FB37E23D68491D6F4"
+        const val EDGE_UA =
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.74 Safari/537.36 Edg/99.0.1150.55"
+        const val EDGE_ORIGIN = "chrome-extension://jdiccldimpdaibmpdkjnbmckianbfold"
+        const val VOICES_LIST_URL =
+            "https://speech.platform.bing.com/consumer/speech/synthesize/readaloud/voices/list?trustedclienttoken=6A5AA1D4EAFF4E9FB37E23D68491D6F4"
+    }
 }
