a3e8a29b821526a42a8f5614e996fd3fcf2167f0
Daniel Kao <daniel.kao@gmail.com>
2025-05-09 22:16:29 +0800

    feat: use stream mode in chat with web

--- app/src/main/java/info/plateaukao/einkbro/browser/ChatWebInterface.kt
+++ app/src/main/java/info/plateaukao/einkbro/browser/ChatWebInterface.kt
@@ -1,17 +1,15 @@
 package info.plateaukao.einkbro.browser
 
+import android.util.Log
 import android.webkit.JavascriptInterface
 import android.webkit.WebView
 import androidx.lifecycle.LifecycleCoroutineScope
 import info.plateaukao.einkbro.preference.ChatGPTActionInfo
 import info.plateaukao.einkbro.preference.ConfigManager
-import info.plateaukao.einkbro.preference.GptActionType
 import info.plateaukao.einkbro.service.ChatMessage
-import info.plateaukao.einkbro.service.ChatRole
 import info.plateaukao.einkbro.service.OpenAiRepository
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
 import kotlin.getValue
@@ -19,27 +17,22 @@ import kotlin.getValue
 class ChatWebInterface(
     private val lifecycleScope: LifecycleCoroutineScope,
     private val webView: WebView,
-    private val webContent: String
-): KoinComponent {
+    private val webContent: String,
+) : KoinComponent {
     private val openAiRepository: OpenAiRepository = OpenAiRepository()
     private val configManager: ConfigManager by inject()
     private val messageList: MutableList<ChatMessage> = mutableListOf()
 
     @JavascriptInterface
     fun sendMessage(message: String) {
-        lifecycleScope.launch(Dispatchers.IO) {
-            val markdownResponse = getMarkdownResponse(message)
-            val escapedResponse = escapeJsString(markdownResponse)
-            withContext(Dispatchers.Main) {
-                webView.evaluateJavascript(
-                    "javascript:receiveMessageFromAndroid('$escapedResponse')",
-                    null
-                )
-            }
+        lifecycleScope.launch(Dispatchers.Main) {
+            //Log.d("ChatWebInterface", "Message: $message")
+            webView.evaluateJavascript("javascript:startMessageStream()", null)
         }
+        generateMarkdownResponse(message)
     }
 
-    private suspend fun getMarkdownResponse(message: String): String {
+    private fun generateMarkdownResponse(message: String) {
         val userPrompt = if (messageList.isEmpty()) {
             "```$webContent```\n this is the web content; $message"
         } else {
@@ -52,18 +45,39 @@ class ChatWebInterface(
             model = configManager.getGptTypeModelMap()[configManager.gptForChatWeb] ?: configManager.gptModel,
         )
 
-        if (chatGptActionInfo.actionType != GptActionType.Gemini) {
-            val completion = openAiRepository.chatCompletion(messageList, chatGptActionInfo)
-            if (completion?.choices?.firstOrNull()?.message != null) {
-                messageList.add(completion.choices.firstOrNull()?.message!!)
+        openAiRepository.chatStream(messageList, chatGptActionInfo,
+            { response ->
+                lifecycleScope.launch(Dispatchers.Main) {
+                    //Log.d("ChatWebInterface", "Response: $response")
+                    webView.evaluateJavascript(
+                        "javascript:receiveMessageFromAndroid('${escapeJsString(response)}', true, false)",
+                        null
+                    )
+                }
+            },
+            doneAction = {
+                lifecycleScope.launch(Dispatchers.Main) {
+                    //Log.d("ChatWebInterface", "Done")
+                    webView.evaluateJavascript("javascript:receiveMessageFromAndroid('', true, true)", null)
+                }
+            },
+            failureAction = {
+                Log.e("ChatWebInterface", "Error in stream")
             }
+        )
 
-            return completion?.choices?.firstOrNull { it.message.role == ChatRole.Assistant }?.message?.content
-                ?: "Something went wrong."
-        } else {
-            val result = openAiRepository.queryGemini(messageList, chatGptActionInfo)
-            return result
-        }
+//        if (chatGptActionInfo.actionType != GptActionType.Gemini) {
+//            val completion = openAiRepository.chatCompletion(messageList, chatGptActionInfo)
+//            if (completion?.choices?.firstOrNull()?.message != null) {
+//                messageList.add(completion.choices.firstOrNull()?.message!!)
+//            }
+//
+//            return completion?.choices?.firstOrNull { it.message.role == ChatRole.Assistant }?.message?.content
+//                ?: "Something went wrong."
+//        } else {
+//            val result = openAiRepository.queryGemini(messageList, chatGptActionInfo)
+//            return result
+//        }
     }
 
     /**
