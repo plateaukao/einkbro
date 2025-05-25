b612c1cebcf9821620fd9bec95207680848c4688
Daniel Kao <daniel.kao@gmail.com>
2025-05-26 01:08:44 +0800

    refactor: browser controller function grouping

--- app/src/main/java/info/plateaukao/einkbro/browser/BrowserController.kt
+++ app/src/main/java/info/plateaukao/einkbro/browser/BrowserController.kt
@@ -13,107 +13,131 @@ import info.plateaukao.einkbro.view.EBWebView
 import info.plateaukao.einkbro.viewmodel.TRANSLATE_API
 
 interface BrowserController {
+    //region Tab and Album Management
     fun updateProgress(progress: Int)
     fun updateTitle(title: String?)
     fun addNewTab(url: String)
-    fun isAtTop(): Boolean
-    fun isCurrentAlbum(albumController: AlbumController): Boolean
-    fun showAlbum(albumController: AlbumController)
-
-    // showHomePage: true -> show home page if it's the last tab
     fun removeAlbum(albumController: AlbumController, showHomePage: Boolean = false)
     fun removeAlbum()
     fun updateAlbum(url: String?)
-    fun showFileChooser(filePathCallback: ValueCallback<Array<Uri>>)
-    fun onShowCustomView(view: View?, callback: CustomViewCallback?)
-    fun onLongPress(message: Message, event: MotionEvent?)
-    fun hideOverview()
-    fun addHistory(title: String, url: String)
-    fun onHideCustomView(): Boolean
-    fun handleKeyEvent(event: KeyEvent): Boolean
-    fun loadInSecondPane(url: String): Boolean //void updateTabs(Album album);
-
-    // for menu actions
-    fun handleTtsButton()
-    fun showTtsLanguageDialog()
-    fun showFastToggleDialog()
-    fun toggleSplitScreen(url: String? = null)
-    fun showTranslation(webView: EBWebView? = null)
-    fun saveBookmark(url: String? = null, title: String? = null)
-    fun createShortcut()
-    fun showSaveEpubDialog()
-    fun showFontSizeChangeDialog()
-    fun showSearchPanel()
-    fun showWebArchiveFilePicker()
-    fun showOpenEpubFilePicker()
-
-    //for tool bar long click
-    fun openHistoryPage(amount: Int = 0)
-
-    //
-    fun showTranslationConfigDialog(translateDirectly: Boolean)
-
-    fun showTouchAreaDialog()
-    fun toggleFullscreen()
-
-    // toolbar click
-    fun focusOnInput()
-    fun handleBackKey()
-    fun refreshAction()
-    fun toggleTouchTurnPageFeature()
-    fun toggleSwitchTouchAreaAction()
-    fun showOverview()
-    fun showMenuDialog()
-    fun openBookmarkPage()
-    fun increaseFontSize()
-    fun decreaseFontSize()
-    fun rotateScreen()
+    fun isCurrentAlbum(albumController: AlbumController): Boolean
+    fun showAlbum(albumController: AlbumController)
     fun newATab()
     fun duplicateTab()
-    fun toggleReaderMode()
-    fun toggleVerticalRead()
-
-    // ereader activity
-    fun showTocDialog() = Unit
-
-    // gesture
     fun gotoLeftTab()
     fun gotoRightTab()
-    fun goForward()
+    //endregion
 
+    //region View Management
+    fun onShowCustomView(view: View?, callback: CustomViewCallback?)
+    fun onHideCustomView(): Boolean
+    fun hideOverview()
+    fun showOverview()
+    fun toggleFullscreen()
+    fun toggleSplitScreen(url: String? = null)
+    //endregion
+
+    //region History and Bookmarks
+    fun addHistory(title: String, url: String)
+    fun saveBookmark(url: String? = null, title: String? = null)
+    fun openHistoryPage(amount: Int = 0)
+    fun openBookmarkPage()
+    //endregion
+
+    //region File Handling
+    fun showFileChooser(filePathCallback: ValueCallback<Array<Uri>>)
+    fun showSaveEpubDialog()
+    fun showWebArchiveFilePicker()
+    fun showOpenEpubFilePicker()
+    //endregion
+
+    //region Input Handling
+    fun onLongPress(message: Message, event: MotionEvent?)
+    fun handleKeyEvent(event: KeyEvent): Boolean
+    fun handleBackKey()
+    fun focusOnInput()
+    //endregion
+
+    //region Navigation
+    fun isAtTop(): Boolean
+    fun loadInSecondPane(url: String): Boolean //void updateTabs(Album album);
+    fun goForward()
     fun jumpToTop()
     fun jumpToBottom()
     fun pageDown()
     fun pageUp()
-    fun updatePageInfo(info: String)
+    fun refreshAction()
+    //endregion
 
+    //region Text-to-Speech (TTS)
+    fun handleTtsButton()
+    fun showTtsLanguageDialog()
+    //endregion
+
+    //region Translation
+    fun showTranslation(webView: EBWebView? = null)
+    fun showTranslationConfigDialog(translateDirectly: Boolean)
+    fun translate(translationMode: TranslationMode)
+    fun resetTranslateUI()
+    fun configureTranslationLanguage(translateApi: TRANSLATE_API)
+    //endregion
+
+    //region Page Content Interaction
+    fun toggleReaderMode()
+    fun toggleVerticalRead()
+    fun increaseFontSize()
+    fun decreaseFontSize()
+    fun showFontSizeChangeDialog()
+    fun showFontBoldnessDialog()
+    fun invertColors()
+    fun updateSelectionRect(left: Float, top: Float, right: Float, bottom: Float)
+    //endregion
+
+    //region Sharing and External Services
+    fun createShortcut()
+    fun addToPocket(url: String)
+    fun handlePocketRequestToken(requestToken: String)
+    fun sendToRemote(text: String)
+    fun shareLink()
+    //endregion
+
+    //region Search
+    fun showSearchPanel()
+    fun toggleTextSearch()
+    fun toggleReceiveTextSearch()
+    //endregion
+
+    //region E-Reader Specific
+    fun showTocDialog() = Unit // Specific to EReaderActivity, default implementation
+    //endregion
+
+    //region Gestures and Touch Controls
+    fun toggleTouchTurnPageFeature()
+    fun toggleSwitchTouchAreaAction()
+    fun showTouchAreaDialog()
+    fun toggleTouchPagination()
+    //endregion
+
+    //region Page Information
+    fun updatePageInfo(info: String)
+    //endregion
+
+    //region Key Sending
     fun sendPageUpKey()
     fun sendPageDownKey()
     fun sendLeftKey()
     fun sendRightKey()
+    //endregion
 
-    fun addToPocket(url: String)
-    fun handlePocketRequestToken(requestToken: String)
-
-    fun translate(translationMode: TranslationMode)
-
-    fun resetTranslateUI()
-
-    fun configureTranslationLanguage(translateApi: TRANSLATE_API)
-    fun toggleTouchPagination()
-    fun showFontBoldnessDialog()
-
-    fun sendToRemote(text: String)
-    fun toggleReceiveLink()
-    fun toggleTextSearch()
-    fun toggleReceiveTextSearch()
-
+    //region AI Features
     fun summarizeContent()
     fun chatWithWeb(useSplitScreen: Boolean = false, content: String? = null, runWithAction: ChatGPTActionInfo? = null)
+    //endregion
 
-    fun updateSelectionRect(left: Float, top: Float, right: Float, bottom: Float)
-
-    fun invertColors()
-
-    fun shareLink()
+    //region UI Toggles and Dialogs
+    fun showFastToggleDialog()
+    fun showMenuDialog()
+    fun rotateScreen()
+    fun toggleReceiveLink()
+    //endregion
 }
