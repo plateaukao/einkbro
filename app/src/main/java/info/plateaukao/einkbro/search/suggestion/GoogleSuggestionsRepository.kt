382a0ef41487f60df5d8b6077c16c7fa899c914c
Daniel Kao <daniel.kao@gmail.com>
2025-09-29 17:22:20 +0800

    feat: add google suggestions repository

---
+++ app/src/main/java/info/plateaukao/einkbro/search/suggestion/GoogleSuggestionsRepository.kt
@@ -0,0 +1,68 @@
+package info.plateaukao.einkbro.search.suggestion
+
+import info.plateaukao.einkbro.EinkBroApplication
+import info.plateaukao.einkbro.R
+import okhttp3.HttpUrl
+import okhttp3.ResponseBody
+import org.xmlpull.v1.XmlPullParser
+import org.xmlpull.v1.XmlPullParserFactory
+
+class GoogleSuggestionsRepository : SearchSuggestionsRepository {
+    private val okHttpClient: okhttp3.OkHttpClient by lazy { okhttp3.OkHttpClient() }
+
+    private val searchSubtitle = EinkBroApplication.instance.getString(R.string.suggestion)
+
+    // https://suggestqueries.google.com/complete/search?output=toolbar&hl={language}&q={query}
+    private fun createQueryUrl(query: String, language: String): HttpUrl = HttpUrl.Builder()
+        .scheme("https")
+        .host("suggestqueries.google.com")
+        .encodedPath("/complete/search")
+        .addQueryParameter("output", "toolbar")
+        .addQueryParameter("hl", language)
+        .addEncodedQueryParameter("q", query)
+        .build()
+
+    @Throws(Exception::class)
+    private fun parseResults(responseBody: ResponseBody): List<SearchSuggestion> {
+        parser.setInput(responseBody.byteStream(), UTF8)
+
+        val suggestions = mutableListOf<SearchSuggestion>()
+        var eventType = parser.eventType
+        while (eventType != XmlPullParser.END_DOCUMENT) {
+            if (eventType == XmlPullParser.START_TAG && "suggestion" == parser.name) {
+                val suggestion = parser.getAttributeValue(null, "data")
+                suggestions.add(SearchSuggestion("$searchSubtitle \"$suggestion\"", suggestion))
+            }
+            eventType = parser.next()
+        }
+
+        return suggestions
+    }
+
+    override suspend fun searchSuggestionResults(query: String): List<SearchSuggestion> {
+        val url = createQueryUrl(query, "null")
+        val request = okhttp3.Request.Builder()
+            .url(url)
+            .get()
+            .build()
+
+        okhttp3.OkHttpClient().newCall(request).execute().use { response ->
+            if (!response.isSuccessful) {
+                throw Exception("Unexpected code $response")
+            }
+
+            val body = response.body ?: throw Exception("Response body is null")
+            return parseResults(body)
+        }
+    }
+
+    companion object {
+        // Converting to a lambda results in pulling the newInstance call out of the lazy.
+        @Suppress("ConvertLambdaToReference")
+        private val parser by lazy {
+            XmlPullParserFactory.newInstance().apply {
+                isNamespaceAware = true
+            }.newPullParser()
+        }
+    }
+}
