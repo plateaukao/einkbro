601a2b9ff76bd55801f1c46a48c8be5e307040cd
Daniel Weck <daniel.weck@gmail.com>
2025-11-05 18:33:25 +0000

    code cleanup and minor UX improvements.

--- app/src/main/assets/zoom-text-wrap-reflow.js
+++ app/src/main/assets/zoom-text-wrap-reflow.js
@@ -44,7 +44,7 @@ if (!document.getElementById(INJECTED_STYLE_ELEMENT_ID)) {
     outline-color: black !important;
     outline-style: dashed !important;
     outline-width: 2px !important;
-    outline-offset: 2px !important;
+    outline-offset: -2px !important;
 }
 
 :root body .${INJECTED_STYLE_CLASS_UP},
@@ -54,11 +54,7 @@ if (!document.getElementById(INJECTED_STYLE_ELEMENT_ID)) {
     outline-color: black !important;
     outline-style: dotted !important;
     outline-width: 4px !important;
-    outline-offset: 4px !important;
-
-    word-wrap: break-word !important;
-    overflow-wrap: break-word !important;
-    max-width: var(--text-reflow-wrap-max-width-var) !important;
+    outline-offset: -4px !important;
 }
 
 :root body .${INJECTED_STYLE_CLASS_FIT},
@@ -67,7 +63,15 @@ if (!document.getElementById(INJECTED_STYLE_ELEMENT_ID)) {
 :root[style] body[style] .${INJECTED_STYLE_CLASS_FIT} {
     word-wrap: break-word !important;
     overflow-wrap: break-word !important;
+
     max-width: var(--text-reflow-wrap-max-width-var) !important;
+
+    margin: 0 !important;
+
+    padding-left: 1em !important;
+    padding-right: 1em !important;
+    padding-top: 1em !important;
+    padding-bottom: 1em !important;
 }
 
 :root body .${INJECTED_STYLE_CLASS_MARGIN},
@@ -82,6 +86,10 @@ if (!document.getElementById(INJECTED_STYLE_ELEMENT_ID)) {
 }
 
 const _touchDowns = [];
+
+let timeout_INJECTED_STYLE_CLASS_MARGIN_add = undefined;
+let timeout_INJECTED_STYLE_CLASS_MARGIN_remove = undefined;
+let timeout_INJECTED_STYLE_CLASS_UP = undefined;
 
 function onPointerDown(ev) {
     console.log("POINTER DOWN ", ev.pointerId, " -- ", ev.x, " , ", ev.y, " >> ", JSON.stringify(_touchDowns, null, 4));
@@ -193,16 +201,54 @@ function onPointerUp(ev) {
     const element = firstNearestCommonAncestor(_touchDowns.map((item) => item.target)) || _touchDowns[0].target;
 
     element.classList.add(INJECTED_STYLE_CLASS_UP);
+    if (!!timeout_INJECTED_STYLE_CLASS_UP) {
+        window.clearTimeout(timeout_INJECTED_STYLE_CLASS_UP);
+        timeout_INJECTED_STYLE_CLASS_UP = undefined;
+    }
+    timeout_INJECTED_STYLE_CLASS_UP = setTimeout(() => {
+        timeout_INJECTED_STYLE_CLASS_UP = undefined;
+        const elementsUp = document.documentElement.querySelectorAll(`.${INJECTED_STYLE_CLASS_UP}`);
+        elementsUp?.forEach((element) => {
+            element.classList.remove(INJECTED_STYLE_CLASS_UP);
+        });
+    }, 800);
 
     const maxWidth = Math.round(window.visualViewport.width * 0.96);
     document.documentElement.style.setProperty('--text-reflow-wrap-max-width-var', `${maxWidth}px`);
 
+//    const rect = element.getBoundingClientRect();
+//    let delay = 0;
+//    if (rect.width < maxWidth) {
+//        delay = 1000;
+//        const ratio = maxWidth / rect.width;
+//        console.log("ZOOM ratio: ", ratio);
+//        document.documentElement.style.zoom = ratio;
+//    }
+
     element.classList.add(INJECTED_STYLE_CLASS_FIT);
 
-    element.classList.add(INJECTED_STYLE_CLASS_MARGIN);
-    element.scrollIntoView({ behavior: 'instant', block: 'nearest', inline: 'start' });
-    setTimeout(() => { element.classList.remove(INJECTED_STYLE_CLASS_MARGIN); }, 500);
+    if (!!timeout_INJECTED_STYLE_CLASS_MARGIN_add) {
+        window.clearTimeout(timeout_INJECTED_STYLE_CLASS_MARGIN_add);
+        timeout_INJECTED_STYLE_CLASS_MARGIN_add = undefined;
+    }
+    timeout_INJECTED_STYLE_CLASS_MARGIN_add = setTimeout(() => {
+        timeout_INJECTED_STYLE_CLASS_MARGIN_add = undefined;
+
+        element.classList.add(INJECTED_STYLE_CLASS_MARGIN);
+        element.scrollIntoView({ behavior: 'instant', block: 'nearest', inline: 'start' });
+
+        if (!!timeout_INJECTED_STYLE_CLASS_MARGIN_remove) {
+            window.clearTimeout(timeout_INJECTED_STYLE_CLASS_MARGIN_remove);
+            timeout_INJECTED_STYLE_CLASS_MARGIN_remove = undefined;
+        }
+        timeout_INJECTED_STYLE_CLASS_MARGIN_remove = setTimeout(() => {
+            timeout_INJECTED_STYLE_CLASS_MARGIN_remove = undefined;
+            element.classList.remove(INJECTED_STYLE_CLASS_MARGIN);
+        }, 500);
+    }, 500);
 }
+
+//window.visualViewport.addEventListener("resize", onVisualViewportResize);
 
 window.document.addEventListener("pointerdown", onPointerDown, true);
 
@@ -214,142 +260,3 @@ window.document.addEventListener("pointercancel", onPointerUp, true);
 //window.document.addEventListener("pointerleave", onPointerUp, true);
 
 })();
-
-//const xpathSelector = `
-////p |
-////a[normalize-space(text())] |
-////h1 |
-////h2 |
-////h3 |
-////h4 |
-////h5 |
-////h6 |
-////li |
-////pre |
-////div[b or em or i] |
-////div[normalize-space(text())] |
-////div[span[normalize-space(text())]]
-//`;
-//
-//const allTextElements = new Set();
-
-//function textReflowWrapToVisibleViewportWidth(targetElement) {
-//
-//    if (!document.getElementById("")) {
-//    const styleContent = `.text-reflow-userscript { word-wrap: break-word !important; overflow-wrap:break-word !important; max-width:var(--text-reflow-max-width) !important; }
-//    .text-reflow-scroll-padding {scroll-margin-left: 1vw !important;}`;
-//    const styleElement = document.createElement('style');
-//    styleElement.textContent = styleContent;
-//    document.head.appendChild(styleElement);
-//    isCssInjected = true;
-//    }
-//
-//    const maxAllowedWidth = Math.round(window.visualViewport.width * 0.96);
-//    document.documentElement.style.setProperty('--text-reflow-max-width', `${'$'}{maxAllowedWidth}px`);
-//
-//    // Select elements likely to contain text
-//    const xpathResult = document.evaluate(xpathSelector, document, null, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null);
-//    allTextElements.clear();
-//
-//    for (let i = 0, n = xpathResult.snapshotLength, el; i < n; i++) {
-//    el = xpathResult.snapshotItem(i);
-//
-//    if (!el.offsetParent) continue;
-//    if (!el.textContent.trim()) continue;
-//
-//    // Proccess only top-level text elements
-//    let isTopLevel = true;
-//    let parent = el.parentElement;
-//
-//    while (parent) {
-//    if (elementIsTextElement(parent)) {
-//    isTopLevel = false;
-//    break;
-//    }
-//    parent = parent.parentElement;
-//    }
-//    if (isTopLevel) {
-//    // Apply CSS styles to element and skip it next time
-//    el.classList.add('text-reflow-userscript');
-//    allTextElements.add(el);
-//    }
-//    }
-//
-//    /// Scroll initial target element into view
-//    if (zoomTarget && targetDyOffsetRatio) {
-//    // Scroll to element vertically, according to new page layout
-//    const targetOffset = targetDyOffsetRatio * window.innerHeight;
-//    const rect = zoomTarget.getBoundingClientRect();
-//    const targetTop = rect.top + window.pageYOffset;
-//    const scrollToPosition = targetTop - targetOffset;
-//
-//    window.scrollTo({
-//    top: scrollToPosition,
-//    behavior: 'instant'
-//    });
-//
-//    // Scroll element into view horizontally
-//    // if (elementIsTextElement(zoomTarget)) {
-//    if (zoomTarget.nodeName !== 'IMG' && zoomTarget.nodeName !== 'VIDEO' && zoomTarget.nodeName !== 'IFRAME'){
-//    zoomTarget.classList.add('text-reflow-scroll-padding')
-//    zoomTarget.scrollIntoView({ behavior: 'smooth', block: 'nearest', inline: 'start' });
-//    zoomTarget.classList.remove('text-reflow-scroll-padding')
-//    }
-//
-//    // Reset the target and offset after scrolling
-//    zoomTarget = null;
-//    targetDyOffsetRatio = null;
-//    }
-//}
-
-//function elementIsTextElement(element) {
-//return allTextElements.has(element);
-//}
-//
-//// Detect start of multi-touch (pinch) gesture
-//function handleTouchStart(event) {
-//if (event.touches && event.touches.length >= 2) {
-//isPinching = true;
-//
-//// Store possible target of zoom gesture
-//if (event.target) zoomTarget = event.target;
-//
-//// Calculate the midpoint between the two touch points
-//const touch1 = event.touches[0];
-//const touch2 = event.touches[1];
-//const midpointX = (touch1.clientX + touch2.clientX) / 2;
-//const midpointY = (touch1.clientY + touch2.clientY) / 2;
-//
-//// Use document.elementFromPoint to get the element at the midpoint
-//let possibleZoomTarget;
-//const elementsFromPoint = document.elementsFromPoint(midpointX, midpointY);
-//for (let i = 0, n = elementsFromPoint.length, element; i < n; i++) {
-//element = elementsFromPoint[i];
-//if (elementIsTextElement(element)) {
-//possibleZoomTarget = element;
-//break;
-//}
-//}
-//if (!possibleZoomTarget) possibleZoomTarget = elementsFromPoint[0];
-//if (possibleZoomTarget) zoomTarget = possibleZoomTarget;
-//
-//// Store screen coordinates of target to scroll it into view after reflow
-//const targetRect= zoomTarget.getBoundingClientRect();
-//targetDyOffsetRatio = targetRect.top / window.innerHeight;
-//}
-//}
-//
-//// Detect end of multi-touch (pinch) gesture
-//function handleTouchEnd(event) {
-//if (isPinching && (event.touches && event.touches.length === 0)) {
-//isPinching = false;
-//reflowText();
-//}
-//}
-//
-//// Add event listeners
-//window.addEventListener('touchstart', handleTouchStart);
-//window.addEventListener('touchend', handleTouchEnd);
-
-/// Uncomment to test on PC
-// window.visualViewport.addEventListener('resize', reflowText);
